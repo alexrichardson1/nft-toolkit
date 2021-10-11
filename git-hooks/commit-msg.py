6aaa149760de133ff7243ab9d530f4a3daa06113
R, Alex <alex.richardson19@imperial.ac.uk>
2021-10-11 16:22:37 +0100

    chore(git-hooks/): Add git hooks

---
+++ git-hooks/commit-msg.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+# pylint: disable=C0103
+# pylint: disable=R1729
+"""
+Used to validate the commit message
+"""
+
+import sys
+import re
+from colorama import Fore, Style
+
+
+def exit_failure(error_message):
+    """Exits non-zero with error message."""
+    print(
+        Fore.RED +
+        Style.BRIGHT +
+        "fatal: " +
+        error_message +
+        Style.RESET_ALL)
+    print("-------------------------------")
+    sys.exit(1)
+
+
+def co_authors(last_line):
+    """Used as a quick way to use co-authors"""
+    # add authors here
+    authors = {"AR": "Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>"}
+    line = last_line.strip("\n").split(" ")
+    print(Fore.GREEN + Style.BRIGHT +
+          "Replacing name(s) with co-author(s)" + Style.RESET_ALL)
+    for i in range(1, len(line)):
+        try:
+            line[i] = authors[line[i]]
+        except KeyError:
+            line[i] = ""
+    line = [x for x in line if x != ""]
+    # return updated authors without "W/"
+    return "\n".join(line[1:])
+
+
+def follows_convention(first_line):
+    """Checks if `first_line` follows commit convention"""
+    # located in the commit template
+    types = ["feat", "fix", "style", "refactor",
+             "perf", "test", "docs", "chore", "build", "ci"]
+    match = re.match
+    if all([not first_line.startswith(type) for type in types]):
+        exit_failure("invalid type.")
+    scope = r"[a-z]+\(\.?[\w\-/]+((\.[a-zA-Z]+)?)+\)"
+    if match(scope, first_line) is None:
+        exit_failure("invalid scope.")
+    if match(scope + r": [A-Z]", first_line) is None:
+        exit_failure("invalid subject.")
+    header_length = len(first_line)
+    if header_length > 72:
+        exit_failure(
+            f"header is longer than 72 characters (+{header_length - 72}).")
+    if any([first_line.endswith(punc) for punc in [
+            ".", "!", "?", ",", "...", ":", ";", "(", ")", "'", "-"]]):
+        exit_failure("trailing punctuation.")
+
+
+def update_commit_msg(file):
+    """Validates and updates the commit message."""
+    new_commit_message = []
+    with open(file, "r") as fp:
+        lines = fp.readlines()
+        if not lines:
+            exit_failure("empty commit message.")
+        follows_convention(lines[0].lstrip())
+        new_commit_message_append = new_commit_message.append
+        for line in lines:
+            line_startswith = line.startswith
+            # remove leading whitespace
+            if line != "\n":
+                line = line.lstrip()
+            # ignore comments
+            if line_startswith("#"):
+                continue
+            # co-author alias
+            if line_startswith("W/"):
+                line = co_authors(line)
+            new_commit_message_append(line)
+    fp.close()
+    return new_commit_message
+
+
+def write_commit_msg(file, new_commit_message):
+    """Write `new_commit_message` to `file`."""
+    with open(file, "w") as fp:
+        fp.writelines(new_commit_message)
+    fp.close()
+
+
+def main():
+    """Main function for the commit-msg hook."""
+    print("--- Running commit-msg hook ---")
+    file = sys.argv[1]
+    write_commit_msg(file, update_commit_msg(file))
+    print(Fore.GREEN + Style.BRIGHT +
+          "commit-msg hook finished successfully." + Style.RESET_ALL)
+    print("-------------------------------")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ git-hooks/pre-commit.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+# pylint: disable=C0103
+"""
+pre-commit hook is used to inspect the snapshot that’s about to be committed
+"""
+
+import sys
+import os
+import subprocess
+from colorama import Fore, Back, Style
+
+
+def exit_failure(error_message):
+    """Exits non-zero with error message."""
+    print(
+        Fore.RED +
+        Style.BRIGHT +
+        "fatal: " +
+        error_message +
+        Style.RESET_ALL)
+    print("-------------------------------")
+    sys.exit(1)
+
+
+def run_formatter(file, formatter):
+    """Format and stage file."""
+    print("Formatting file: " + Back.BLACK + file + Style.RESET_ALL)
+    os.system(formatter + file)
+    os.system("git add " + file)
+
+
+def format_files(staged_files, file_extension, formatter):
+    """Format `staged_files` with the extension `file_extension` using the command `formatter`."""
+    for file in staged_files:
+        if file.endswith(file_extension):
+            run_formatter(file, formatter)
+
+
+def format_python(files):
+    """Formats python files"""
+    format_files(files, ".py", "autopep8 -i ")
+
+
+def run_linter(file, linter):
+    """Lint `file` using the command `linter`."""
+    print("Linting file: " + Back.BLACK + file + Style.RESET_ALL)
+    if os.WEXITSTATUS(os.system(linter + file)) != 0:
+        exit_failure("linting failed")
+
+
+def lint_files(staged_files, file_extension, linter):
+    """Lint `staged_files` with the extension `file_extension` using the command `linter`."""
+    for file in staged_files:
+        if file.endswith(file_extension):
+            run_linter(file, linter)
+
+
+def lint_python(files):
+    """Lint python files."""
+    lint_files(files, ".py", "pylint ")
+
+
+def main():
+    """Main function for the pre-commit hook."""
+    print("--- Running pre-commit hook ---")
+    files = subprocess.check_output(
+        "git diff --name-only --staged",
+        shell=True,
+        universal_newlines=True)
+    staged_files = files.split("\n")
+    files = subprocess.check_output(
+        "git diff --name-only --diff-filter=D",
+        shell=True,
+        universal_newlines=True)
+    deleted_filles = files.split("\n")
+    staged_files = [
+        file for file in staged_files if file not in deleted_filles]
+    # add format functions here
+    format_functions = [format_python]  # e.g. [format_java, format_haskell]
+    for format_function in format_functions:
+        format_function(staged_files)
+    # add lint functions here
+    lint_functions = [lint_python]  # e.g. [lint_java, lint_haskell]
+    for lint_function in lint_functions:
+        lint_function(staged_files)
+    print(Fore.GREEN + Style.BRIGHT +
+          "pre-commit hook finished successfully." + Style.RESET_ALL)
+    print("-------------------------------")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
