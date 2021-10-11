3c4bc262a3ee4cc12fbd28c4ac13cc5553573d63
R, Alex <alex.richardson19@imperial.ac.uk>
2021-10-11 16:22:37 +0100

    chore(git-hooks/): Add git hooks

---
+++ git-hooks/install-hooks.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Run the command `chmod +x ./install-hooks.bash` in the scripts directory
+
+HOOK_DIR=git-hooks
+GIT_DIR=$(git rev-parse --git-dir)
+
+chmod +x pre-commit.py commit-msg.py
+
+echo "Installing hooks..."
+git config --local commit.template $HOOK_DIR/commit_template.txt
+# symlinks to the hook scripts
+ln -sf $GIT_DIR/../$HOOK_DIR/pre-commit.py $GIT_DIR/hooks/pre-commit
+ln -sf $GIT_DIR/../$HOOK_DIR/commit-msg.py $GIT_DIR/hooks/commit-msg
+echo "Done!"
