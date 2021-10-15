0f99462459eecf301bcf8ea323692ead74a671b1
Reece Jackson <Rjackson2000@icloud.com>
2021-10-15 22:14:19 +0100

    chore(index.tsx): Add `CssBaseline` from MUI
    Co-authored-by: Aayush Dalal <aayush.dalal19@imperial.ac.uk>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,11 @@
-// eslint-disable-next-line no-use-before-define
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/App";
+import CssBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
