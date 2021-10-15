1dfd0f3d37d1332dbf23243e12c6bd348bdabd45
Reece Jackson <Rjackson2000@icloud.com>
2021-10-16 00:32:43 +0100

    feat(Icons.tsx): Add feature to show current network
    Co-authored-by: Aayush Dalal <aayush.dalal19@imperial.ac.uk>

--- client/src/components/common/navbar/Icons.tsx
+++ client/src/components/common/navbar/Icons.tsx
@@ -1,32 +1,42 @@
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
-// import { ReactComponent as Ethereum } from "../../../images/ethereum-logo.svg";
 import { ReactComponent as Ethereum } from "../../../images/ethereum-logo.svg";
 import { ReactComponent as Cardano } from "../../../images/cardano-logo.svg";
 import { ReactComponent as Polygon } from "../../../images/polygon-logo.svg";
 import { ReactComponent as Solana } from "../../../images/solana-logo.svg";
 
+const createIcon = (
+  Component: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & {
+      title?: string | undefined;
+    }
+  >
+) => <Component width="70%" height="70%" />;
+
 const actions = [
   {
-    icon: <Ethereum width="70%" height="70%" />,
+    icon: createIcon(Ethereum),
     name: "Ethereum Network",
   },
   {
-    icon: <Polygon width="70%" height="70%" />,
+    icon: createIcon(Polygon),
     name: "Polygon Network",
   },
   {
-    icon: <Solana width="70%" height="70%" />,
+    icon: createIcon(Solana),
     name: "Solana Network",
   },
   {
-    icon: <Cardano width="70%" height="70%" />,
+    icon: createIcon(Cardano),
     name: "Cardano Network",
   },
 ];
 
 export default function OpenIconSpeedDial(): JSX.Element {
+  const [selectedNet, setSelectedNet] = useState(actions[0]);
+  // useEffect(, [selectedLogo])
   return (
     <Box
       sx={{
@@ -40,12 +50,13 @@ export default function OpenIconSpeedDial(): JSX.Element {
         ariaLabel="SpeedDial openIcon example"
         sx={{ position: "absolute", top: 0 }}
         direction={"down"}
-        icon={<Polygon width="70%" height="70%" />}>
+        icon={selectedNet.icon}>
         {actions.map((action) => (
           <SpeedDialAction
             key={action.name}
             icon={action.icon}
             tooltipTitle={action.name}
+            onClick={() => setSelectedNet(action)}
           />
         ))}
       </SpeedDial>
