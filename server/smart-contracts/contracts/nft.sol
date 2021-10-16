e48f5b961eea00fbe4f5cf69d2de8e88ba869214
R, Alex <alex.richardson19@imperial.ac.uk>
2021-10-16 11:48:24 +0100

    feat(nft.sol): Add `mint` function

--- server/smart-contracts/contracts/nft.sol
+++ server/smart-contracts/contracts/nft.sol
@@ -4,19 +4,37 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721URIStorage {
+  uint256 private _price;
   uint256 private _limit;
+  using Counters for Counters.Counter;
+  Counters.Counter public _tokenIdTracker;
 
   constructor(
     string memory name,
     string memory symbol,
-    uint256 limit
+    uint256 limit,
+    uint256 price
   ) ERC721(name, symbol) {
     _limit = limit;
+    _price = price;
   }
 
   function setTokenURI(uint256 tokenId, string memory _tokenURI) external {
     _setTokenURI(tokenId, _tokenURI);
+  }
+
+  function mint(uint256 amount) public payable {
+    require(msg.value == _price * amount, "Must send correct price");
+    require(
+      _tokenIdTracker.current() + amount <= _limit,
+      "Not enough in the collection left to mint amount"
+    );
+    for (uint256 i = 0; i < amount; i++) {
+      _mint(msg.sender, _tokenIdTracker.current());
+      _tokenIdTracker.increment();
+    }
   }
 }
