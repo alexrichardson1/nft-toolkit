06293e0e4610b41fe6d83fb02bef2e2f46dd6e5e
Jaimi Patel <jaimip34@gmail.com>
2021-10-16 01:36:37 +0100

    feat(controllers/collection.ts): Add saving collection to db

--- server/src/controllers/collection.ts
+++ server/src/controllers/collection.ts
@@ -3,12 +3,21 @@ import { createReadStream } from "fs";
 import { PutObjectCommand, S3 } from "@aws-sdk/client-s3";
 import path from "path";
 import { NextFunction, Request, Response, RequestHandler } from "express";
+import { Collection, Token } from "../models/collection";
 
 export const uploadImagesLocal = multer({ dest: "uploads/" }).any();
 
-// TODO: Complete CollectionT
+interface TokenT {
+  name: string;
+  description: string;
+  image: string;
+}
+
 interface CollectionT {
   name: string;
+  symbol: string;
+  description: string;
+  tokens: TokenT[];
 }
 
 const s3 = new S3({
@@ -40,9 +49,23 @@ export const uploadImagesS3: RequestHandler = (
     next();
   }
   const { files } = req;
+  // TODO: Add error handling
   const fileArray = files as Express.Multer.File[];
   fileArray.map((file, id) =>
     uploadToBucket(collection.name, file.path, id + 1)
   );
+  next();
+};
+
+export const saveCollectionToDB: RequestHandler = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  const collection: CollectionT = req.body;
+  collection.tokens.map((token) => new Token(token));
+  const mongoCollection = new Collection(collection);
+  // TODO: Add error handling
+  await mongoCollection.save();
   next();
 };
