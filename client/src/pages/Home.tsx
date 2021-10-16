10ccac763377c6dfc440a3bc61e39f5e44288b8f
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-17 00:01:56 +0100

    feat(Home.tsx): Add initial home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Navbar from "components/common/Navbar";
+
+const Home = (): JSX.Element => {
+  return (
+    <>
+      <Navbar />
+      Hello world!
+    </>
+  );
+};
+
+export default Home;
