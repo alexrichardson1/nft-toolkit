22fa91fac359813975d84d6207a454161b212017
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-17 00:29:24 +0100

    feat(App.tsx): Initial start on mui theme

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "context/theme/ThemeContext";
 import Home from "pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = (): JSX.Element => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 
---
+++ client/src/context/theme/ThemeContext.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+import { purple, yellow } from "@mui/material/colors";
+
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: purple,
+    secondary: yellow,
+  },
+});
