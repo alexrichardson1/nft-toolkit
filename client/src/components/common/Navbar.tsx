cddc1fc174ea61f3a04cefff397a6981b2cee439
Reece Jackson <Rjackson2000@icloud.com>
2021-10-16 14:37:35 +0100

    chore(Navbar.tsx): Move to common and remove separate file for styles
    Co-authored-by: Aayush Dalal <aayush.dalal19@imperial.ac.uk>

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import OpenIconSpeedDial from "components/common/Networks";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
@@ -9,13 +10,29 @@ import Menu from "@mui/material/Menu";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import Toolbar from "@mui/material/Toolbar";
-import {
-  MenuItemStyle,
-  AppBarStyle,
-  ToolbarStyle,
-  OptionsStyle,
-} from "./NavbarStyles";
-import OpenIconSpeedDial from "./Icons";
+
+const anchorOrigin: AnchorOriginType = { vertical: "top", horizontal: "right" };
+
+const toolbarStyle = { justifyContent: "center", alignItems: "center" };
+
+const moreIconContainerStyle = { display: { xs: "flex", md: "none" } };
+
+const fabStyle = { padding: "15px", display: "flex", gap: "5px" };
+
+const menuItemStyle = { display: "flex", gap: "5px" };
+
+const appBarStyle = {
+  height: "80px",
+  display: "flex",
+  justifyContent: "center",
+};
+
+const optionsStyle = {
+  display: { xs: "none", md: "flex" },
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "15px",
+};
 
 const Navbar = (): JSX.Element => {
   const [mobileAnchorEl, setMobileAnchorEl] = useState<AnchorType>(null);
@@ -30,52 +47,45 @@ const Navbar = (): JSX.Element => {
     setMobileAnchorEl(event.currentTarget);
   };
 
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
+  const MobileMenu = () => (
     <Menu
       anchorEl={mobileAnchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={mobileMenuId}
+      anchorOrigin={anchorOrigin}
+      transformOrigin={anchorOrigin}
+      id={"primary-menu-mobile"}
       keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}>
-      <MenuItem sx={MenuItemStyle} onClick={() => console.log("Clicked this")}>
+      <MenuItem sx={menuItemStyle} onClick={() => console.log("Clicked this")}>
         <AccountBalanceWalletIcon /> Connect Wallet
       </MenuItem>
     </Menu>
   );
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar sx={AppBarStyle} position="sticky">
-        <Toolbar sx={ToolbarStyle}>
-          <Typography variant="h3" noWrap component="div">
-            NFT-TOOLKIT
+    <Box flexGrow={1}>
+      <AppBar sx={appBarStyle} position="sticky">
+        <Toolbar sx={toolbarStyle}>
+          <Typography variant="h4" noWrap component="div">
+            NFTit
           </Typography>
           <Box flexGrow={1} />
-          <Box sx={OptionsStyle}>
+          <Box sx={optionsStyle}>
             <Fab
               variant="extended"
               color="primary"
               aria-label="add"
-              sx={{ padding: "15px", display: "flex", gap: "5px" }}>
+              sx={fabStyle}>
               <AccountBalanceWalletIcon />
               Connect Wallet
             </Fab>
             <OpenIconSpeedDial />
           </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <Box sx={moreIconContainerStyle}>
             <IconButton
               size="large"
               aria-label="show more"
-              aria-controls={mobileMenuId}
+              aria-controls={"primary-menu-mobile"}
               aria-haspopup="true"
               onClick={handleMobileMenuOpen}
               color="inherit">
@@ -84,7 +94,7 @@ const Navbar = (): JSX.Element => {
           </Box>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
+      <MobileMenu />
     </Box>
   );
 };
--- client/src/lib/global.d.ts
+++ client/src/lib/global.d.ts
@@ -1,1 +1,7 @@
 type AnchorType = null | HTMLElement;
+type VertPositionType = number | "top" | "center" | "bottom";
+type HorizontalPositionType = number | "left" | "center" | "right";
+type AnchorOriginType = {
+  vertical: VertPositionType;
+  horizontal: HorizontalPositionType;
+};
