14ab9522f88c4c1de1b4a71fad706f4a2d14444c
Reece Jackson <Rjackson2000@icloud.com>
2021-10-16 14:23:44 +0100

    chore(constants.js): Add a constants file
    Co-authored-by: Aayush Dalal <aayush.dalal19@imperial.ac.uk>

---
+++ client/src/common/constants.ts
@@ -0,0 +1,19 @@
+import EthereumLogo from "images/ethereum-logo.svg";
+import CardanoLogo from "images/cardano-logo.svg";
+import PolygonLogo from "images/polygon-logo.svg";
+import SolanaLogo from "images/solana-logo.svg";
+import BinanceLogo from "images/binance-logo.svg";
+import AvalancheLogo from "images/avalanche-logo.svg";
+
+const logos = [
+  EthereumLogo,
+  CardanoLogo,
+  AvalancheLogo,
+  SolanaLogo,
+  PolygonLogo,
+  BinanceLogo,
+];
+
+const names = ["Ethereum", "Cardano", "Avalanche", "Solana", "Polygon", "BSC"];
+
+export { logos, names };
