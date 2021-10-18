3e847d612bcf6213311bc612542bf4c782455268
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-18 19:07:39 +0100

    chore(src/): Extract ThemeProvider to `index.tsx`
    Co-authored-by: Reece Jackson <Rjackson2000@icloud.com>

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,16 +1,13 @@
-import ThemeProvider from "context/theme/ThemeProvider";
 import Home from "pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = (): JSX.Element => {
   return (
-    <ThemeProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 };
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/App";
 import CssBaseline from "@mui/material/CssBaseline";
+import ThemeProvider from "context/theme/ThemeProvider";
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
