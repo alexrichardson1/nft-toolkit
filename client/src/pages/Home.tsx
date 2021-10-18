d93de4a1928128ebfd1ec7f47a86377ba80d4af7
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-18 20:06:34 +0100

    feat(Home.tsx): Add Form component to `Home.tsx`
    Co-authored-by: Reece Jackson <Rjackson2000@icloud.com>

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,13 @@
-import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 import Navbar from "components/common/Navbar";
+import Controls from "components/form/controls/Controls";
 
 const Home = (): JSX.Element => {
   return (
     <>
       <Navbar />
-      <Typography>Hello world!</Typography>
+      <Divider />
+      <Controls />
     </>
   );
 };
