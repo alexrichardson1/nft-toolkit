7b7439e2d43ff4e3c09699d22cc3903fecbfae2b
Jaimi Patel <jaimip34@gmail.com>
2021-10-20 23:54:23 +0100

    feat(index.tsx): Add `DAppProvider`
    Co-authored-by: Aayush Dalal <aayush.dalal19@imperial.ac.uk>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,16 @@ import App from "components/App";
 import CssBaseline from "@mui/material/CssBaseline";
 import ThemeProvider from "context/theme/ThemeProvider";
 import NetworkProvider from "context/network/NetworkProvider";
+import { DAppProvider } from "@usedapp/core";
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
     <ThemeProvider>
       <NetworkProvider>
-        <App />
+        <DAppProvider config={{}}>
+          <App />
+        </DAppProvider>
       </NetworkProvider>
     </ThemeProvider>
   </React.StrictMode>,
