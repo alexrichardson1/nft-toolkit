05bc25497a2299593bee4af77a02156071da05d2
Morkus Salasevicius <salaseviciusmorkus@gmail.com>
2021-10-20 16:13:48 +0100

    test(jest.config.js): Initial commit
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

---
+++ server/jest.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line tsdoc/syntax
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
