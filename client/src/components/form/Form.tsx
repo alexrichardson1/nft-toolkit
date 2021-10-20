e59179af08473a4a9bcf994267ae1523236201df
Reece Jackson <Rjackson2000@icloud.com>
2021-10-20 19:36:53 +0100

    feat(formReducer.tsx): Add reducer and context for inputs
    Co-authored-by: Aayush Dalal <aayush.dalal19@imperial.ac.uk>

--- client/src/components/form/Form.tsx
+++ client/src/components/form/Form.tsx
@@ -1,68 +1,129 @@
 import Input from "./Input";
 import Tabs from "./Tabs";
+import formReducer from "./formReducer";
+import NetworkContext from "context/network/NetworkContext";
 import ImageUpload from "./custom-image-upload/ImageUpload";
 import InputAdornment from "@mui/material/InputAdornment";
 import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
 import DoneIcon from "@mui/icons-material/Done";
 import ClearIcon from "@mui/icons-material/Clear";
 import Container from "@mui/material/Container";
-import NetworkContext from "context/network/NetworkContext";
-import { useContext, useState } from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import { FormEvent, useContext, useReducer } from "react";
 import { getNetworkFromName } from "common/constants";
+import Box from "@mui/material/Box";
 
 const ICON_SIZE = 25;
 
+const initialState = {
+  collectionName: "",
+  description: "",
+  images: [],
+  mintingPrice: 0,
+};
+
 const Form = (): JSX.Element => {
-  const [files, setFiles] = useState<File[]>([]);
   const { selectedNet } = useContext(NetworkContext);
   const networkIcon = getNetworkFromName(selectedNet, ICON_SIZE).icon;
 
+  const [state, dispatch] = useReducer(formReducer, initialState);
+
   return (
-    <Container>
-      <Input
-        label="Collection Name"
-        value=""
-        placeholder="Please enter your collection name"
-        required
-      />
-      <Input
-        rows={4}
-        label="Desciption"
-        value=""
-        placeholder="Please enter a description"
-        multiline
-        required
-      />
-      <ImageUpload setFiles={setFiles} />
-      <Tabs files={files} />
-      <Input
-        label="Minting Price"
-        type="number"
-        value=""
-        placeholder="Enter minting price"
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">{networkIcon}</InputAdornment>
-          ),
-        }}
-      />
-      <ButtonGroup>
-        <Button
-          startIcon={<ClearIcon />}
-          color="error"
-          size="large"
-          variant="contained">
-          Cancel
-        </Button>
-        <Button
-          endIcon={<DoneIcon />}
-          color="success"
-          size="large"
-          variant="contained">
-          Submit
-        </Button>
-      </ButtonGroup>
+    <Container
+      onSubmit={(e: FormEvent) => {
+        e.preventDefault();
+        console.log(state);
+      }}
+      component={"form"}>
+      <Grid justifyContent="center" spacing={2} direction="column" container>
+        <Grid item md={12}>
+          <Paper>
+            <Input
+              label="Collection Name"
+              value={state.collectionName}
+              placeholder="Please enter your collection name"
+              onChange={(e) =>
+                dispatch({ type: "CHANGE_NAME", payload: e.target.value })
+              }
+              required
+            />
+          </Paper>
+        </Grid>
+        <Grid item md={12}>
+          <Paper>
+            <Input
+              rows={4}
+              label="Description"
+              value={state.description}
+              placeholder="Please enter a description"
+              multiline
+              required
+              onChange={(e) =>
+                dispatch({
+                  type: "CHANGE_DESCRIPTION",
+                  payload: e.target.value,
+                })
+              }
+            />
+          </Paper>
+        </Grid>
+        <Grid item md={12}>
+          <Paper>
+            <ImageUpload files={state.images} dispatch={dispatch} />
+          </Paper>
+        </Grid>
+        {state.images.length > 0 && (
+          <Grid item md={12}>
+            <Paper>
+              <Tabs files={state.images} />
+            </Paper>
+          </Grid>
+        )}
+        <Grid item md={12}>
+          <Paper>
+            <Input
+              label="Minting Price"
+              required
+              type="number"
+              onChange={(e) =>
+                dispatch({ type: "CHANGE_PRICE", payload: e.target.value })
+              }
+              value={state.mintingPrice}
+              placeholder="Enter minting price"
+              InputProps={{
+                inputProps: { min: 0 },
+                endAdornment: (
+                  <InputAdornment position="end">{networkIcon}</InputAdornment>
+                ),
+              }}
+            />
+          </Paper>
+        </Grid>
+        <Grid item md={12}>
+          <Box sx={{ display: "flex" }}>
+            <Box flexGrow={1} />
+            <Box sx={{ display: "flex", gap: "10px" }}>
+              <Button
+                startIcon={<ClearIcon />}
+                color="error"
+                size="large"
+                variant="contained"
+                type="reset">
+                Reset
+              </Button>
+              <Button
+                type="submit"
+                endIcon={<DoneIcon />}
+                color="success"
+                size="large"
+                variant="contained">
+                Submit
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
---
+++ client/src/components/form/formReducer.tsx
@@ -0,0 +1,34 @@
+const formReducer = (state: FormStateT, action: formActionI): FormStateT => {
+  switch (action.type) {
+    case "CHANGE_NAME":
+      return {
+        ...state,
+        collectionName: action.payload as string,
+      };
+    case "CHANGE_DESCRIPTION":
+      return {
+        ...state,
+        description: action.payload as string,
+      };
+    case "CHANGE_IMAGES":
+      return {
+        ...state,
+        images: [
+          ...state.images,
+          ...(action.payload as File[]).map((image) => ({
+            image,
+            url: URL.createObjectURL(image),
+          })),
+        ],
+      };
+    case "CHANGE_PRICE":
+      return {
+        ...state,
+        mintingPrice: parseFloat(action.payload as string),
+      };
+    default:
+      return state;
+  }
+};
+
+export default formReducer;
