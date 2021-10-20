736aef3d9cbb6a47ae40020d4e901d78ccfe2712
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-20 13:21:31 +0100

    feat(Form.tsx): Add `tabs` component to form
    Co-authored-by: Reece Jackson <Rjackson2000@icloud.com>
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- client/src/components/form/Form.tsx
+++ client/src/components/form/Form.tsx
@@ -1,5 +1,66 @@
+import Input from "./Input";
+import Tabs from "./Tabs";
+import ImageUpload from "./custom-image-upload/ImageUpload";
+import InputAdornment from "@mui/material/InputAdornment";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import DoneIcon from "@mui/icons-material/Done";
+import ClearIcon from "@mui/icons-material/Clear";
+import Container from "@mui/material/Container";
+import { useState } from "react";
+
 const Form = (): JSX.Element => {
-  return <div></div>;
+  const [files, setFiles] = useState<File[]>([]);
+  return (
+    <Container>
+      <Input
+        label="Collection Name"
+        value=""
+        placeholder="Please enter your collection name"
+        required
+      />
+      <Input
+        rows={4}
+        label="Desciption"
+        value=""
+        placeholder="Please enter a description"
+        multiline
+        required
+      />
+      <ImageUpload setFiles={setFiles} />
+      <Tabs files={files} />
+      <Input
+        label="Minting Price"
+        type="number"
+        value=""
+        placeholder="Enter minting price"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <ButtonGroup>
+        <Button
+          startIcon={<ClearIcon />}
+          color="error"
+          size="large"
+          variant="contained">
+          Cancel
+        </Button>
+        <Button
+          endIcon={<DoneIcon />}
+          color="success"
+          size="large"
+          variant="contained">
+          Submit
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
 };
 
 export default Form;
---
+++ client/src/components/form/Tabs.tsx
@@ -0,0 +1,113 @@
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { useState } from "react";
+import { getComponentByMode } from "common/constants";
+
+const tabPanelStyle = { padding: 3 };
+
+const vTabsContainerStyle = {
+  flexGrow: 1,
+  bgcolor: "background.paper",
+  display: "flex",
+  height: 224,
+  border: 1,
+  borderRadius: 1,
+};
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const TabPanel = (props: TabPanelProps) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}>
+      {value === index && (
+        <Box sx={tabPanelStyle}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+const a11yProps = (index: number) => {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+};
+
+interface PropsT {
+  files: File[];
+}
+
+const VerticalTabs = (props: PropsT): JSX.Element => {
+  const [value, setValue] = useState(0);
+
+  const IMAGE_3 = 2;
+  const IMAGE_4 = 3;
+  const IMAGE_5 = 4;
+  const IMAGE_6 = 5;
+
+  return (
+    <Box sx={vTabsContainerStyle}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        allowScrollButtonsMobile
+        selectionFollowsFocus
+        value={value}
+        onChange={(_e, newValue) => setValue(newValue)}
+        TabScrollButtonProps={{
+          sx: {
+            color: (theme) =>
+              getComponentByMode(theme.palette.mode, "#121212", "white"),
+          },
+        }}
+        sx={{ borderRight: 1, borderColor: "divider" }}>
+        <Tab label="Image 1" {...a11yProps(0)} />
+        <Tab label="Image 2" {...a11yProps(1)} />
+        <Tab label="Image 3" {...a11yProps(IMAGE_3)} />
+        <Tab label="Image 4" {...a11yProps(IMAGE_4)} />
+        <Tab label="Image 5" {...a11yProps(IMAGE_5)} />
+        <Tab label="Image 6" {...a11yProps(IMAGE_6)} />
+      </Tabs>
+      {props.files.map((_file, idx) => (
+        <TabPanel key={idx} value={value} index={idx}>
+          Image {idx}
+        </TabPanel>
+      ))}
+      <TabPanel value={value} index={0}>
+        Img One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Img Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Img Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Img Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Img Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Img Six
+      </TabPanel>
+    </Box>
+  );
+};
+
+export default VerticalTabs;
