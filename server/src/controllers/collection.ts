11b4ac0fe4734d76409f0d4564f6c521620f3b72
Jaimi Patel <jaimip34@gmail.com>
2021-10-22 00:46:32 +0100

    feat(controllers/collection.ts): Add deployContracts
    - Add collection to user model
    - Add image url to token
    - Set S3 image to public access
    - Generate transaction for contract

--- server/src/controllers/collection.ts
+++ server/src/controllers/collection.ts
@@ -1,9 +1,12 @@
 import { PutObjectCommand, S3 } from "@aws-sdk/client-s3";
+import { ethers } from "ethers";
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import { createReadStream } from "fs";
 import multer from "multer";
 import path from "path";
+import { NFT__factory as NftFactory } from "../../smart-contracts/typechain";
 import { Collection, Token } from "../models/collection";
+import { User } from "../models/user";
 
 export const uploadImagesLocal = multer({ dest: "uploads/" }).any();
 
@@ -17,6 +20,8 @@ interface CollectionT {
   name: string;
   symbol: string;
   description: string;
+  price: number;
+  address?: string;
   tokens: TokenT[];
 }
 
@@ -29,13 +34,17 @@ const s3 = new S3({
 
 const uploadToBucket = (collection: string, filePath: string, id: number) => {
   const fileStream = createReadStream(filePath);
+  const imageKey = `${collection}/${id}${path.extname(filePath)}`;
   const uploadParams = {
     Bucket: "nft-toolkit-collections",
     // Save the image to the collection folder using id to name it.
-    Key: `${collection}/${id}${path.extname(filePath)}`,
+    Key: imageKey,
     Body: fileStream,
+    ACL: "public-read",
   };
   s3.send(new PutObjectCommand(uploadParams));
+  const imageURL = `https://nft-toolkit-collections.s3.eu-west-2.amazonaws.com/${imageKey}`;
+  return imageURL;
 };
 
 export const uploadImagesS3: RequestHandler = (
@@ -44,28 +53,65 @@ export const uploadImagesS3: RequestHandler = (
   next: NextFunction
 ) => {
   const collection: CollectionT = req.body;
-  if (!req.files) {
-    // TODO: Add error message?
-    next();
-  }
   const { files } = req;
-  // TODO: Add error handling
+  if (!files) {
+    return next(new Error("File array undefined"));
+  }
   const fileArray = files as Express.Multer.File[];
-  fileArray.map((file, id) =>
-    uploadToBucket(collection.name, file.path, id + 1)
-  );
-  next();
+  fileArray.map((file, id) => {
+    const imageURL = uploadToBucket(collection.name, file.path, id + 1);
+    const currentToken = collection.tokens[id];
+    if (currentToken) {
+      currentToken.image = imageURL;
+    }
+  });
+  return next();
 };
+
+interface UserT {
+  fromAddress: string;
+}
 
 export const saveCollectionToDB: RequestHandler = async (
   req: Request,
   _res: Response,
   next: NextFunction
 ) => {
-  const collection: CollectionT = req.body;
-  collection.tokens.map((token) => new Token(token));
-  const mongoCollection = new Collection(collection);
+  const userCollection: CollectionT & UserT = req.body;
+  userCollection.tokens.map((token) => new Token(token));
+  const collection = new Collection(userCollection);
+  let user = await User.findOne({
+    fromAddress: userCollection.fromAddress,
+  }).exec();
+  if (user) {
+    user.collections.push(collection);
+  } else {
+    user = new User({
+      fromAddress: userCollection.fromAddress,
+      collections: [collection],
+    });
+  }
+  await user.save();
   // TODO: Add error handling
-  await mongoCollection.save();
+  next();
+};
+
+export const deployContracts: RequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const collection: CollectionT & UserT = req.body;
+  const signer = new ethers.VoidSigner(collection.fromAddress);
+  const NFTContract = new NftFactory(signer);
+  const tx = NFTContract.getDeployTransaction(
+    collection.name,
+    collection.symbol,
+    // TODO: baseURI link
+    "",
+    collection.tokens.length,
+    collection.price
+  );
+  res.json({ transaction: tx });
   next();
 };
