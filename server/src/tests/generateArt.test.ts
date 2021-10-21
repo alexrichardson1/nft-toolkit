f97280524c9fe67b647f294eb1f2cbca9efaf479
Morkus Salasevicius <salaseviciusmorkus@gmail.com>
2021-10-21 15:15:34 +0100

    test(generateArt.test.ts): Generate() produces randomised art
    - Extract rigging of Math.Random() function to a helper function
    `makeMathRandomReturn()`

--- server/src/tests/generateArt.test.ts
+++ server/src/tests/generateArt.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-magic-numbers */
 import { generate, GenCollectionI } from "../controllers/generateArt";
 
 describe("Generate Art", () => {
@@ -41,14 +42,16 @@ describe("Generate Art", () => {
     jest.clearAllMocks();
   });
 
-  test("Produces black background, fat body, durag head", () => {
-    testData.quantity = 1;
-    // eslint-disable-next-line no-magic-numbers
-    const vals = [0.02, 0.45, 0.08, 0.18, 0.08, 0.96];
+  function setMathRandomReturn(vals: number[]): void {
     let i = 0;
     Math.random = () => {
       return vals[i++] || 0;
     };
+  }
+
+  test("Produces black background, fat body, durag head", () => {
+    testData.quantity = 1;
+    setMathRandomReturn([0.02, 0.45, 0.08, 0.18, 0.08, 0.96]);
     const expected = [
       [
         { name: "black", rarity: 30 },
@@ -58,6 +61,30 @@ describe("Generate Art", () => {
     ];
     const result = generate(testData);
     expect(result).toMatchObject(expected);
+  });
+
+  test("Different random values produce different combinations", () => {
+    testData.quantity = 2;
+    setMathRandomReturn([
+      // red, normal, bald
+      0.0, 0.3, 0.0, 0.905, 0.0, 0.6999,
+      // black, skinny, spiky
+      0.0, 0.6, 0.0, 0.5, 0.0, 0.95,
+    ]);
+    const expected = [
+      [
+        { name: "red", rarity: 30 },
+        { name: "normal", rarity: 10 },
+        { name: "bald", rarity: 70 },
+      ],
+      [
+        { name: "black", rarity: 30 },
+        { name: "skinny", rarity: 70 },
+        { name: "spiky", rarity: 25 },
+      ],
+    ];
+
+    expect(generate(testData)).toMatchObject(expected);
   });
 
   // test("TODO", () => {
