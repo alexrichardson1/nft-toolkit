5e77125d038705a82ab32fa2c86897b8176f5677
Jaimi Patel <jaimip34@gmail.com>
2021-10-22 00:05:52 +0100

    feat(index.ts): Use `express.json()`

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,12 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // Routes to handle requests
 app.use("/collection", collectionRoutes);
