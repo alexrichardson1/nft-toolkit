0ba55e2f8850c421a0fa39ae0eab3610b63fefaa
R, Alex <alex.richardson19@imperial.ac.uk>
2021-10-21 13:06:18 +0100

    test(nft.test.ts): Add tests for `mint` functions

--- server/smart-contracts/test/nft.test.ts
+++ server/smart-contracts/test/nft.test.ts
@@ -1,18 +1,17 @@
-import chai from "chai";
+import chai, { expect } from "chai";
 import { ethers } from "hardhat";
 import { solidity } from "ethereum-waffle";
-// import { Contract } from "ethers";
 import { NFT } from "../typechain/NFT";
 
 chai.use(solidity);
-const { expect } = chai;
 
 const collectionSize = 10;
-const collectionWeiPrice = 1000000000;
+const collectionWeiPrice = ethers.utils.parseEther("1");
 
 describe("NFT Collection Contract", function () {
   let nftContract: NFT;
-  before(async function () {
+
+  before(async () => {
     const NFTContract = await ethers.getContractFactory("NFT");
     nftContract = await NFTContract.deploy(
       "Monkeys",
@@ -22,12 +21,32 @@ describe("NFT Collection Contract", function () {
     );
     await nftContract.deployed();
   });
-  describe("Constructor", function () {
-    it("Should set name correctly", async function () {
+
+  describe("Constructor", () => {
+    it("Should set name correctly", async () => {
       expect(await nftContract.name()).to.equal("Monkeys");
     });
-    it("Should set symbol correctly", async function () {
+
+    it("Should set symbol correctly", async () => {
       expect(await nftContract.symbol()).to.equal("MNKY");
+    });
+  });
+
+  describe("Mint", () => {
+    it("Should not allow minting without the correct funds", () => {
+      expect(
+        nftContract.mint(1, {
+          value: ethers.utils.parseEther("0"),
+        })
+      ).to.be.revertedWith("Must send correct price");
+    });
+
+    it("Should not allow minting more than the collection size", () => {
+      expect(
+        nftContract.mint(collectionSize + 1, {
+          value: collectionWeiPrice.mul(collectionSize + 1),
+        })
+      ).to.be.revertedWith("Not enough in the collection left to mint amount");
     });
   });
 });
