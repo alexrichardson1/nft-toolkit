51c455ecadf21497ed017b4073f49c254fd349f7
R, Alex <alex.richardson19@imperial.ac.uk>
2021-10-21 16:34:03 +0100

    refactor(src/): Extract server code
    Co-authored-by: Jaimi Patel <jaimip34@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,14 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 import collectionRoutes from "./routes/collection";
 
-dotenv.config();
 const app = express();
-// default port to listen
-const port = 5000;
 
 const corsOptions = {
   origin: "http://localhost:3000",
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-
-mongoose.connect(process.env.DB_URI);
 
 // Routes to handle requests
 app.use("/collection", collectionRoutes);
@@ -24,11 +17,6 @@ app.use("/collection", collectionRoutes);
 app.get("/", (_req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.send("Hello world!");
-});
-
-// start the Express server
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
 });
 
 export default app;
