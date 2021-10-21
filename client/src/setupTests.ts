337e03adfeb7bdc95d6d6430aa91da6ef6d40d23
Reece Jackson <Rjackson2000@icloud.com>
2021-10-21 22:47:47 +0100

    test(setupTests.ts): Add the setup for `enzyme`
    Co-authored-by: Aayush Dalal <aayush.dalal19@imperial.ac.uk>
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { configure } from "enzyme";
+
+configure({ adapter: new Adapter() });
