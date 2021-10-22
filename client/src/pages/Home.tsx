a520bcec30232d8411bd051847404ba064053422
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-22 23:28:53 +0100

    chore(Home.tsx): Remove `Divider` component
    Co-authored-by: Reece Jackson <Rjackson2000@icloud.com>

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
 import Form from "components/form/Form";
 import { mainContainerStyle } from "utils/constants";
 
 const Home = (): JSX.Element => {
   return (
     <>
-      <Divider />
       <Box sx={mainContainerStyle}>
         <Form />
       </Box>
