49e05485b11a951328994b2471ee45e40415d408
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-22 22:41:24 +0100

    feat(Form.tsx): Add alert on submission and reset for form

--- client/src/components/form/Form.tsx
+++ client/src/components/form/Form.tsx
@@ -1,20 +1,26 @@
 import ClearIcon from "@mui/icons-material/Clear";
+import CloseIcon from "@mui/icons-material/Close";
 import DoneIcon from "@mui/icons-material/Done";
+import { Alert, AlertColor, Collapse, IconButton } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import InputAdornment from "@mui/material/InputAdornment";
 import Paper from "@mui/material/Paper";
+import { SxProps } from "@mui/system";
 import SvgLogo from "components/common/SvgLogo";
 import NetworkContext from "context/network/NetworkContext";
-import { FormEvent, useContext, useReducer } from "react";
+import { FormEvent, useContext, useReducer, useState } from "react";
 import formReducer from "reducers/formReducer";
+import { DEFAULT_ALERT_DURATION } from "utils/constants";
+import showAlert from "utils/showAlert";
 import ImageUpload from "./custom-image-upload/ImageUpload";
 import FormGridItem from "./FormGridInput";
 import Tabs from "./tabs/Tabs";
 
 const ICON_SIZE = 25;
+const DESCRIPTION_ROWS = 4;
 const INITIAL_STATE = {
   collectionName: "",
   description: "",
@@ -22,14 +28,23 @@ const INITIAL_STATE = {
   mintingPrice: 0,
 };
 
-const buttonsWrapperStyle = { display: "flex", gap: "10px" };
-const buttonContainerStyle = { display: "flex" };
+const formFooterStyle: SxProps = {
+  display: "flex",
+  gap: "10px",
+  minHeight: 50,
+  flexDirection: { xs: "column", sm: "row" },
+};
+const buttonsWrapperStyle = {
+  justifyContent: "flex-end",
+  display: "flex",
+  gap: "10px",
+};
 
 const priceInputProps = (selectedNet: NetworkT) => ({
   inputProps: { min: "0.00", step: "any" },
   endAdornment: (
     <InputAdornment position="end">
-      {<SvgLogo icon={selectedNet.icon} width={ICON_SIZE} height={ICON_SIZE} />}
+      <SvgLogo icon={selectedNet.icon} width={ICON_SIZE} height={ICON_SIZE} />
     </InputAdornment>
   ),
 });
@@ -37,6 +52,10 @@ const priceInputProps = (selectedNet: NetworkT) => ({
 const Form = (): JSX.Element => {
   const { selectedNet } = useContext(NetworkContext);
   const [state, dispatch] = useReducer(formReducer, INITIAL_STATE);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertSeverity, setAlertSeverity] = useState<AlertColor>("success");
+
+  const closeAlert = () => setAlertMessage("");
 
   const handleImageDelete = (deleteId: string) => {
     dispatch({ type: "DELETE_IMAGE", payload: { deleteId } });
@@ -45,12 +64,7 @@ const Form = (): JSX.Element => {
   const handleImgNameChange = (e: InputEventT, id: string) =>
     dispatch({
       type: "CHANGE_IMAGE_NAME",
-      payload: {
-        newImgObj: {
-          newImageName: e.target.value,
-          imageId: id,
-        },
-      },
+      payload: { newImgObj: { newImageName: e.target.value, imageId: id } },
     });
 
   const handleImageDrop = (
@@ -65,10 +79,7 @@ const Form = (): JSX.Element => {
   };
 
   const handleCollNameChange = (e: InputEventT) =>
-    dispatch({
-      type: "CHANGE_NAME",
-      payload: { newName: e.target.value },
-    });
+    dispatch({ type: "CHANGE_NAME", payload: { newName: e.target.value } });
 
   const handleDescriptionChange = (e: InputEventT) =>
     dispatch({
@@ -77,83 +88,86 @@ const Form = (): JSX.Element => {
     });
 
   const handleMintPriceChange = (e: InputEventT) =>
-    dispatch({
-      type: "CHANGE_PRICE",
-      payload: { price: e.target.value },
-    });
+    dispatch({ type: "CHANGE_PRICE", payload: { price: e.target.value } });
 
-  const handleStateReset = () =>
+  const showFormAlert = (severity: AlertColor, message: string) => {
+    showAlert(setAlertSeverity, severity, setAlertMessage, message);
+    setTimeout(closeAlert, DEFAULT_ALERT_DURATION);
+  };
+
+  const handleStateReset = () => {
     dispatch({ type: "RESET_STATE", payload: { initialState: INITIAL_STATE } });
+    showFormAlert("info", "Form has been reset");
+  };
 
   const handleFormSubmit = (e: FormEvent) => {
     e.preventDefault();
     console.log(state);
+    showFormAlert("error", "You have hit a dead-end");
   };
 
-  const collectionNameGrid = (
-    <FormGridItem
-      value={state.collectionName}
-      onChange={handleCollNameChange}
-      placeholder="Enter a collection name"
-      label="Collection Name"
-    />
+  const alertBox = (
+    <Box flexGrow={1}>
+      <Collapse in={alertMessage.length !== 0}>
+        <Alert
+          elevation={6}
+          variant="filled"
+          severity={alertSeverity}
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={closeAlert}>
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }>
+          {alertMessage}
+        </Alert>
+      </Collapse>
+    </Box>
   );
 
-  const descriptionGrid = (
-    <FormGridItem
-      value={state.description}
-      multiline
-      onChange={handleDescriptionChange}
-      placeholder="Enter a description"
-      rows={4}
-      label="Description"
-    />
-  );
-
-  const mintingGrid = (
-    <FormGridItem
-      value={state.mintingPrice}
-      onChange={handleMintPriceChange}
-      placeholder="Enter a minting price"
-      label="Minting Price"
-      type="number"
-      InputProps={priceInputProps(selectedNet)}
-    />
-  );
-
-  const resetGrid = (
-    <Grid item md={12}>
-      <Box sx={buttonContainerStyle}>
-        <Box flexGrow={1} />
-        <Box sx={buttonsWrapperStyle}>
-          <Button
-            startIcon={<ClearIcon />}
-            color="error"
-            size="large"
-            variant="contained"
-            type="reset"
-            onClick={handleStateReset}>
-            Reset
-          </Button>
-          <Button
-            type="submit"
-            endIcon={<DoneIcon />}
-            color="success"
-            size="large"
-            variant="contained">
-            Submit
-          </Button>
-        </Box>
-      </Box>
-    </Grid>
+  const formButtons = (
+    <Box sx={buttonsWrapperStyle}>
+      <Button
+        startIcon={<ClearIcon />}
+        color="error"
+        size="large"
+        variant="contained"
+        type="reset"
+        onClick={handleStateReset}>
+        Reset
+      </Button>
+      <Button
+        type="submit"
+        endIcon={<DoneIcon />}
+        color="success"
+        size="large"
+        variant="contained">
+        Submit
+      </Button>
+    </Box>
   );
 
   return (
     <Container onSubmit={handleFormSubmit} component="form">
       <Grid justifyContent="center" spacing={2} direction="column" container>
-        {collectionNameGrid}
-        {descriptionGrid}
-        <Grid item md={12}>
+        <FormGridItem
+          value={state.collectionName}
+          onChange={handleCollNameChange}
+          placeholder="Enter a collection name"
+          label="Collection Name"
+        />
+        <FormGridItem
+          value={state.description}
+          multiline
+          onChange={handleDescriptionChange}
+          placeholder="Enter a description"
+          rows={DESCRIPTION_ROWS}
+          label="Description"
+        />
+        <Grid item xs={12}>
           <Paper>
             <ImageUpload
               handleImageDrop={handleImageDrop}
@@ -162,7 +176,7 @@ const Form = (): JSX.Element => {
           </Paper>
         </Grid>
         {state.images.length > 0 && (
-          <Grid item md={12}>
+          <Grid item xs={12}>
             <Paper>
               <Tabs
                 handleImageDelete={handleImageDelete}
@@ -172,8 +186,20 @@ const Form = (): JSX.Element => {
             </Paper>
           </Grid>
         )}
-        {mintingGrid}
-        {resetGrid}
+        <FormGridItem
+          value={state.mintingPrice}
+          onChange={handleMintPriceChange}
+          placeholder="Enter a minting price"
+          label="Minting Price"
+          type="number"
+          InputProps={priceInputProps(selectedNet)}
+        />
+        <Grid item xs={12}>
+          <Box sx={formFooterStyle}>
+            {alertBox}
+            {formButtons}
+          </Box>
+        </Grid>
       </Grid>
     </Container>
   );
