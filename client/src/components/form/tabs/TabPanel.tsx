3ea8345a0405691f2455df5141a61e8b90ef7e34
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-22 14:03:45 +0100

    fix(tabs/): Fix image rendering issue on chrome

--- client/src/components/form/tabs/TabPanel.tsx
+++ client/src/components/form/tabs/TabPanel.tsx
@@ -1,7 +1,18 @@
-import { Box } from "@mui/system";
+import { Box, SxProps, Theme } from "@mui/system";
 import { ReactNode } from "react";
 
 const tabPanelStyle = { width: "100%", height: "100%" };
+
+const tabPanelBoxStyle: SxProps<Theme> = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: 1,
+  padding: 3,
+  height: "100%",
+  width: "100%",
+  overflowY: "scroll",
+};
 
 interface TabPanelProps {
   children?: ReactNode;
@@ -20,21 +31,7 @@ const TabPanel = (props: TabPanelProps): JSX.Element => {
       aria-labelledby={`vertical-tab-${index}`}
       style={tabPanelStyle}
       {...other}>
-      {value === index && (
-        <Box
-          // inline sx required
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: 1,
-            padding: 3,
-            height: "100%",
-            width: "100%",
-          }}>
-          {children}
-        </Box>
-      )}
+      {value === index && <Box sx={tabPanelBoxStyle}>{children}</Box>}
     </div>
   );
 };
