0ffaf9aa957e9895530e7f19250a85cb6798baa7
R, Alex <alex.richardson19@imperial.ac.uk>
2021-10-22 18:45:01 +0100

    chore(install-hooks.bash): Add `pre-push` soft link

--- git-hooks/install-hooks.bash
+++ git-hooks/install-hooks.bash
@@ -5,11 +5,12 @@
 HOOK_DIR=git-hooks
 GIT_DIR=$(git rev-parse --git-dir)
 
-chmod +x pre-commit.py commit-msg.py
+chmod +x pre-commit.py commit-msg.py pre-push.py
 
 echo "Installing hooks..."
 git config --local commit.template $HOOK_DIR/commit_template.txt
 # symlinks to the hook scripts
 ln -sf $GIT_DIR/../$HOOK_DIR/pre-commit.py $GIT_DIR/hooks/pre-commit
 ln -sf $GIT_DIR/../$HOOK_DIR/commit-msg.py $GIT_DIR/hooks/commit-msg
+ln -sf $GIT_DIR/../$HOOK_DIR/pre-push.py $GIT_DIR/hooks/pre-push
 echo "Done!"
