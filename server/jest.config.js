7c8f4b5a9d01f4ad90a2b7cd5d0c6171b3162aff
R, Alex <alex.richardson19@imperial.ac.uk>
2021-10-23 09:06:01 +0100

    test(jest.config.js): Add `coverageThreshold`

--- server/jest.config.js
+++ server/jest.config.js
@@ -5,4 +5,12 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["build"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
