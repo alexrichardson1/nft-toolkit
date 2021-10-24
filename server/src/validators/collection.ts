0f969916375702e6182af79fce557d0df2f247c7
Jaimi Patel <jaimip34@gmail.com>
2021-10-24 16:06:53 +0100

    feat(validators/collection.ts): Add collection validation

---
+++ server/src/validators/collection.ts
@@ -0,0 +1,22 @@
+import { TokenT } from "controllers/collection";
+import { check, CustomValidator, ValidationChain } from "express-validator";
+
+const isValidTokens: CustomValidator = (value: TokenT[]) => {
+  return value.length > 0;
+};
+
+export const collectionValidator: () => ValidationChain[] = () => {
+  return [
+    check("name")
+      .notEmpty()
+      .isAlphanumeric()
+      .withMessage("Invalid name, must be only alphanumeric"),
+    check("symbol").notEmpty().isAlphanumeric(),
+    check("description").notEmpty().escape(),
+    check("price").isNumeric(),
+    check("tokens")
+      .custom(isValidTokens)
+      .withMessage("Must have at least one NFT in collection."),
+    check("fromAddress").isEthereumAddress().withMessage("Invalid address"),
+  ];
+};
