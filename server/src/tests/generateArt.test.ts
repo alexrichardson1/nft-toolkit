d66c825bbf2f262421d1bb44e134887a0246096c
Morkus Salasevicius <salaseviciusmorkus@gmail.com>
2021-10-26 14:22:17 +0100

    test(generateArt.test.ts): Test for duplicates in generated art
    - Test that no duplicates can be formed in the collection, where a
    duplicate NFT is a matching combination of layers and layer images

--- server/src/tests/generateArt.test.ts
+++ server/src/tests/generateArt.test.ts
@@ -113,6 +113,47 @@ describe("Generate Art", () => {
     ]);
   });
 
+  test("No duplicates can exist in the collection", () => {
+    testData.quantity = 2;
+    const PROB1 = 0.0;
+    const PROB2 = 0.3;
+    const PROB3 = 0.0;
+    const PROB4 = 0.905;
+    const PROB5 = 0.0;
+    const PROB6 = 0.6999;
+    const PROB7 = 0.0;
+    const PROB8 = 0.3;
+    const PROB9 = 0.0;
+    const PROB10 = 0.905;
+    const PROB11 = 0.0;
+    const PROB12 = 0.6999;
+
+    setMathRandomReturn([
+      PROB1,
+      PROB2,
+      PROB3,
+      PROB4,
+      PROB5,
+      PROB6,
+      PROB7,
+      PROB8,
+      PROB9,
+      PROB10,
+      PROB11,
+      PROB12,
+    ]);
+
+    const expected = [
+      { name: "red", rarity: 30 },
+      { name: "normal", rarity: 10 },
+      { name: "bald", rarity: 70 },
+    ];
+
+    const collection = generate(testData);
+    expect(collection[0]).toMatchObject(expected);
+    expect(collection[1]).not.toMatchObject(expected);
+  });
+
   // test("TODO", () => {
   //   const vals = Array.from({ length: 30 }, (_, i) => i + 1);
   //   let i = 0;
