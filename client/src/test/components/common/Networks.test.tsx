a455449ccebe73850bf25ccad540845f35d374ba
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-28 23:07:15 +0100

    test(Networks.test.tsx): Test network change

--- client/src/test/components/common/Networks.test.tsx
+++ client/src/test/components/common/Networks.test.tsx
@@ -1,3 +1,4 @@
+import { fireEvent, render } from "@testing-library/react";
 import { DAppProvider } from "@usedapp/core";
 import NetworkSpeedDial from "components/common/Networks";
 import NetworkProvider from "context/network/NetworkProvider";
@@ -14,5 +15,19 @@ test("NetworksSpeedDial snapshot", () => {
       </NetworkProvider>
     </ThemeProvider>
   );
+  expect(tree).toMatchSnapshot();
+});
+
+test("NetworksSpeedDial network changes", () => {
+  const tree = render(
+    <ThemeProvider>
+      <NetworkProvider>
+        <DAppProvider config={{}}>
+          <NetworkSpeedDial />
+        </DAppProvider>
+      </NetworkProvider>
+    </ThemeProvider>
+  );
+  fireEvent.click(tree.getByTestId("Avalanche"));
   expect(tree).toMatchSnapshot();
 });
