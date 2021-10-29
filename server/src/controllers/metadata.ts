0f511abd03475910fa05305f5bb699a5d37d1c06
Jaimi Patel <jaimip34@gmail.com>
2021-10-29 12:26:59 +0100

    feat(controllers/metadata.ts): Remove catch for mongoose query

--- server/src/controllers/metadata.ts
+++ server/src/controllers/metadata.ts
@@ -1,30 +1,31 @@
 import { User } from "@models/user";
 import { RequestHandler } from "express";
 
-export const getTokenMetadata: RequestHandler = (req, res, next) => {
+const FIRST_TOKEN_NO = 1;
+
+export const getTokenMetadata: RequestHandler = async (req, res, next) => {
   const { fromAddress, collectionName, tokenId } = req.params;
   if (!fromAddress || !collectionName || !tokenId) {
     return next(new Error("Invalid params"));
   }
   const tokenNumber = parseInt(tokenId, 10);
-  User.findOne({ fromAddress: fromAddress, collectionName: collectionName })
-    .exec()
-    .then((user) => {
-      if (!user) {
-        return next(new Error("Collection not found"));
-      }
-      const collection = user.collections.find(
-        (col) => col.name === collectionName
-      );
-      if (!collection) {
-        return next(new Error("Collection name not found"));
-      }
-      const token = collection.tokens[tokenNumber - 1];
-      res.json(token);
-      return next();
-    })
-    .catch((err) => {
-      return next(err);
-    });
-  return next();
+  const user = await User.findOne({
+    fromAddress: fromAddress,
+    collectionName: collectionName,
+  }).exec();
+  if (!user) {
+    return next(new Error("User not found"));
+  }
+  const collection = user.collections.find(
+    (col) => col.name === collectionName
+  );
+  if (!collection) {
+    return next(new Error("Collection name not found"));
+  }
+  const numTokens = collection.tokens.length;
+  if (tokenNumber < FIRST_TOKEN_NO || tokenNumber > numTokens) {
+    return next(new Error("Token id not found"));
+  }
+  const token = collection.tokens[tokenNumber - 1];
+  return res.json(token);
 };
