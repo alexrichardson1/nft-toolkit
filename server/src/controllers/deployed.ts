e3a9a1968ed005f1721cf03621816a0d6e9c9064
Jaimi Patel <jaimip34@gmail.com>
2021-10-29 13:37:32 +0100

    feat(controllers/deployed.ts): Add next function for error handling

--- server/src/controllers/deployed.ts
+++ server/src/controllers/deployed.ts
@@ -1,24 +1,23 @@
-import { ERROR_CODE } from "@controllers/common";
 import { User } from "@models/user";
 import { RequestHandler } from "express";
 
-export const addDeployedAddress: RequestHandler = async (req, res) => {
+export const addDeployedAddress: RequestHandler = async (req, res, next) => {
   const { deployedAddress, fromAddress, collectionName } = req.params;
   const user = await User.findOne({
     fromAddress: fromAddress,
   }).exec();
   if (!user) {
-    return res.status(ERROR_CODE).send("Sorry can't find that user");
+    return next(new Error("Sorry can't find that user"));
   }
   const collection = user.collections.find(
     (col) => col.name === collectionName
   );
   if (!collection) {
-    return res.status(ERROR_CODE).send("Sorry can't find that collection");
+    return next(new Error("Sorry can't find that collection"));
   }
   collection.address = deployedAddress;
   user.save().catch((err: Error) => {
-    return res.status(ERROR_CODE).send(err.message);
+    return next(err);
   });
   return res.json({ success: true });
 };
