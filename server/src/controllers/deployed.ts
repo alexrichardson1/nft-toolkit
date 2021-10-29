93355603f7cf9cfd0d4ab3f28e089639adec80ef
Jaimi Patel <jaimip34@gmail.com>
2021-10-29 15:48:31 +0100

    feat(controllers/deployed.ts): Improve error messages

--- server/src/controllers/deployed.ts
+++ server/src/controllers/deployed.ts
@@ -3,17 +3,20 @@ import { RequestHandler } from "express";
 
 export const addDeployedAddress: RequestHandler = async (req, res, next) => {
   const { deployedAddress, fromAddress, collectionName } = req.params;
+  if (!fromAddress || !collectionName || !deployedAddress) {
+    return next(new Error("Invalid params"));
+  }
   const user = await User.findOne({
     fromAddress: fromAddress,
   }).exec();
   if (!user) {
-    return next(new Error("Sorry can't find that user"));
+    return next(new Error("User not found"));
   }
   const collection = user.collections.find(
     (col) => col.name === collectionName
   );
   if (!collection) {
-    return next(new Error("Sorry can't find that collection"));
+    return next(new Error("Collection name not found"));
   }
   collection.address = deployedAddress;
   user.save().catch((err: Error) => {
