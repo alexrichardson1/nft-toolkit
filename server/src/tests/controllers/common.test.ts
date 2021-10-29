4c2c396a69a1f104a6353ec18b93852062e22b5b
Jaimi Patel <jaimip34@gmail.com>
2021-10-29 23:02:24 +0100

    test(common.test.ts): Add mocking of `express-validator` funcs

--- server/src/tests/controllers/common.test.ts
+++ server/src/tests/controllers/common.test.ts
@@ -1,8 +1,15 @@
 import { errorHandler, ERROR_CODE } from "@controllers/common";
-import { collectionValidator } from "@validators/collection";
 import { NextFunction, Request, Response } from "express";
 
-const mockRequest = {} as unknown as Request;
+jest.mock("express-validator", () => {
+  return {
+    validationResult: jest.fn((req) => ({
+      isEmpty: jest.fn(() => !req.body.errors),
+      array: jest.fn(),
+    })),
+  };
+});
+
 let mockResponse: Response;
 let mockNext: NextFunction;
 
@@ -22,16 +29,13 @@ afterEach(() => {
 
 describe("Validator Error Handler", () => {
   it("Should succeed if there are no errors", () => {
+    const mockRequest = { body: { errors: false } } as unknown as Request;
     errorHandler(mockRequest, mockResponse, mockNext);
     expect(mockNext).toHaveBeenCalledWith();
   });
 
-  it("Should send response with error code and errors if validator errors present", async () => {
-    await Promise.all(
-      collectionValidator().map(async (validator) => {
-        await validator(mockRequest, mockResponse, mockNext);
-      })
-    );
+  it("Should send response with error code and errors if validator errors present", () => {
+    const mockRequest = { body: { errors: true } } as unknown as Request;
     errorHandler(mockRequest, mockResponse, mockNext);
     expect(mockResponse.status).toHaveBeenCalledWith(ERROR_CODE);
   });
