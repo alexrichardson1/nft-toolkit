1d634b7ea3689e621fa588712b5b0b025c1cb72b
Jaimi Patel <jaimip34@gmail.com>
2021-11-03 00:58:57 +0000

    feat(walletUtils.tsx): Add `supportedChains`
    - Rename file from `navbarUtils.ts`

--- client/src/components/walletUtils.ts
+++ client/src/components/walletUtils.ts
@@ -6,3 +6,11 @@ export const getAccountString = (account?: string | null): string => {
   const END_CHARS = 4;
   return `${account.slice(0, START_CHARS)}....${account.slice(-END_CHARS)}`;
 };
+
+// TODO: Testnet chain ids
+const ETH_ID = 1;
+const MATIC_ID = 137;
+const AVAX_ID = 43114;
+const BSC_ID = 56;
+
+export const supportedChains = [ETH_ID, MATIC_ID, AVAX_ID, BSC_ID];
