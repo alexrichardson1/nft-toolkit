855cbdd44bccd703ba20b149dee768e6e8ad3214
Jaimi Patel <jaimip34@gmail.com>
2021-11-03 00:46:32 +0000

    chore(navbarUtils.ts): Remove `@usedapp` connectWallet

--- client/src/components/navbar/navbarUtils.ts
+++ client/src/components/navbar/navbarUtils.ts
@@ -1,26 +1,3 @@
-import { AlertColor } from "@mui/material";
-
-export const connectWallet = (
-  deactivate: () => void,
-  showSnackbar: (type: AlertColor, message: string) => void,
-  activateBrowserWallet: (
-    onError?: (error: Error) => void,
-    throwErrors?: boolean
-  ) => void,
-  account?: string | null
-): void => {
-  if (account) {
-    deactivate();
-    return;
-  }
-  activateBrowserWallet();
-  if (account) {
-    showSnackbar("success", "Your wallet has been connected!");
-    return;
-  }
-  showSnackbar("error", "Your wallet was not connected. Please try again.");
-};
-
 export const getAccountString = (account?: string | null): string => {
   if (!account) {
     return "Connect Wallet";
