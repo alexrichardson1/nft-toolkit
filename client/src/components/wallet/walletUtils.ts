8afcbe300f02dae91a8ba82672a6cd819d6cd256
Jaimi Patel <jaimip34@gmail.com>
2021-11-04 19:17:22 +0000

    feat(walletUtils.ts): Add snackbar error and success messages

--- client/src/components/wallet/walletUtils.ts
+++ client/src/components/wallet/walletUtils.ts
@@ -1,4 +1,5 @@
 import { Web3Provider } from "@ethersproject/providers";
+import { AlertColor } from "@mui/material";
 import { NETWORKS } from "utils/constants";
 import {
   HEXADECIMAL,
@@ -17,18 +18,21 @@ export const getAccountString = (account?: string | null): string => {
 
 export const updateNetwork = (
   chainId: number,
-  setSelectedNet: (newNetwork: NetworkT) => void
+  setSelectedNet: (newNetwork: NetworkT) => void,
+  showSnackbar: (type: AlertColor, message: string) => void
 ): void => {
   const selectedNet = NETWORKS.find((net) => net.chainId === chainId);
   if (selectedNet) {
     setSelectedNet(selectedNet);
+    showSnackbar("success", `Connected to ${RPC_URLS[chainId]?.chainName}`);
   }
 };
 
 export const switchChain = (
   network: NetworkT,
   library: Web3Provider | undefined,
-  setNetwork: (network: NetworkT) => void
+  setNetwork: (network: NetworkT) => void,
+  showSnackbar: (type: AlertColor, message: string) => void
 ): void => {
   const { chainId } = network;
   // Able to change network freely if wallet not connected
@@ -41,16 +45,21 @@ export const switchChain = (
     return;
   }
   const hexChainId = `0x${chainId.toString(HEXADECIMAL)}`;
+
   library
     .send("wallet_switchEthereumChain", [{ chainId: hexChainId }])
+    .then(() =>
+      showSnackbar("success", `Connected to ${RPC_URLS[chainId]?.chainName}`)
+    )
     .catch((err) => {
       if (err.code === NOT_ADDED_NETWORK_ERROR) {
         // Adds network to metamask
         const rpcInfo = RPC_URLS[chainId];
-        library.send("wallet_addEthereumChain", [rpcInfo]);
+        library.send("wallet_addEthereumChain", [rpcInfo]).catch((err) => {
+          showSnackbar("error", err.message);
+        });
+      } else {
+        showSnackbar("error", err.message);
       }
-      // TODO: Add network if not in metamask
-      // TODO: Error snackbar
-      console.log(err);
     });
 };
