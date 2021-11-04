8f88c10bd138ec4681cb467a50581cf46f98c961
Jaimi Patel <jaimip34@gmail.com>
2021-11-04 16:30:26 +0000

    test(walletUtils.test.ts): Add `switchChain` tests

--- client/src/test/components/walletUtils.test.ts
+++ client/src/test/components/walletUtils.test.ts
@@ -1,4 +1,9 @@
-import { getAccountString, updateNetwork } from "components/walletUtils";
+import { Web3Provider } from "@ethersproject/providers";
+import {
+  getAccountString,
+  switchChain,
+  updateNetwork,
+} from "components/walletUtils";
 import EthereumLogo from "images/ethereum-logo.svg";
 
 describe("walletUtils unit tests", () => {
@@ -28,5 +33,33 @@ describe("walletUtils unit tests", () => {
     const INVALID_ID = -1;
     updateNetwork(INVALID_ID, setSelectedNet);
     expect(setSelectedNet).toHaveBeenCalledTimes(0);
+  });
+
+  test("Able to change network if undefined library (wallet not connected)", () => {
+    const setNetwork = jest.fn();
+    const mockNetwork = { name: "Polygon", chainId: 137 } as NetworkT;
+    // eslint-disable-next-line no-undefined
+    switchChain(mockNetwork, undefined, setNetwork);
+    expect(setNetwork).toHaveBeenCalledWith(mockNetwork);
+  });
+
+  test("Does not change to non-EVM blockchains if wallet connected", () => {
+    const setNetwork = jest.fn();
+    const mockNetwork = { name: "Cardano" } as NetworkT;
+    switchChain(mockNetwork, {} as Web3Provider, setNetwork);
+    expect(setNetwork).toHaveBeenCalledTimes(0);
+  });
+
+  test("Sends request to switch chain to injected connector", () => {
+    const send = jest.fn(() => ({
+      catch: jest.fn(),
+    }));
+    const mockNetwork = { name: "Polygon", chainId: 137 } as NetworkT;
+    switchChain(
+      mockNetwork,
+      { send: send } as unknown as Web3Provider,
+      jest.fn()
+    );
+    expect(send).toHaveBeenCalledTimes(1);
   });
 });
