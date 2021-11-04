db67ce8722822797efe46dd65ecc4e78aabc0137
Jaimi Patel <jaimip34@gmail.com>
2021-11-04 21:48:54 +0000

    feat(Wallet.tsx): Remove library polling interval

--- client/src/components/wallet/Wallet.tsx
+++ client/src/components/wallet/Wallet.tsx
@@ -17,7 +17,6 @@ export function getLibrary(
   provider: ExternalProvider | JsonRpcFetchFunc
 ): Web3Provider {
   const library = new Web3Provider(provider);
-  library.pollingInterval = 12000;
   return library;
 }
 
