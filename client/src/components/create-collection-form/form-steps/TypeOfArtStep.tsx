b632bd78292827fa67a76196f7c3daa623467fb1
Reece Jackson <Rjackson2000@icloud.com>
2021-11-04 22:46:39 +0000

    feat(TypeOfArtStep.tsx): Design frontend for each button
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- client/src/components/create-collection-form/form-steps/TypeOfArtStep.tsx
+++ client/src/components/create-collection-form/form-steps/TypeOfArtStep.tsx
@@ -2,46 +2,87 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import genArtImg from "images/generativeIcon.svg";
+import staticArtImg from "images/staticIcon.svg";
+import { LinkContainer } from "react-router-bootstrap";
 
 const TypeOfArtStep = (): JSX.Element => {
   return (
-    <Grid container spacing={5}>
-      <Grid item xs={10} md={4}>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            "& > :not(style)": {
-              margin: 1,
-              width: 400,
-              height: 400,
-            },
-          }}>
-          <Button
-            sx={{
-              background:
-                'url("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg")',
-              backgroundSize: "100 400",
-              backgroundRepeat: "no-repeat",
-            }}
-            variant="outlined"></Button>
-        </Box>
+    <Grid container justifyContent="center" spacing={4} alignItems="center">
+      <Grid item xs={10} md={5}>
+        <Button>
+          <Paper elevation={5}>
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                "& > :not(style)": {
+                  margin: 1,
+                  width: 400,
+                  height: 500,
+                },
+              }}>
+              <LinkContainer to="/">
+                <Box>
+                  <Typography align="center" variant="h4" component="div">
+                    Generative Art
+                  </Typography>
+                  <img
+                    style={{
+                      height: 300,
+                      width: 300,
+                    }}
+                    src={genArtImg}
+                  />
+                  <Typography variant="body1" component="div">
+                    Dynamically generate an NFT collection of multi-layered
+                    images. Select the layers you want each image to include,
+                    Upload your pictures with a rarity for each one. Finally,
+                    let us generate your NFTs via smart contracts and deploy the
+                    collection!
+                  </Typography>
+                </Box>
+              </LinkContainer>
+            </Box>
+          </Paper>
+        </Button>
       </Grid>
-      <Grid item xs={10} md={4}>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            "& > :not(style)": {
-              margin: 1,
-              width: 400,
-              height: 400,
-            },
-          }}>
-          <Button variant="outlined">
-            <Paper elevation={3}></Paper>
-          </Button>
-        </Box>
+      <Grid item xs={10} md={5}>
+        <Button>
+          <Paper elevation={5}>
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                "& > :not(style)": {
+                  margin: 1,
+                  width: 400,
+                  height: 500,
+                },
+              }}>
+              <LinkContainer to="/">
+                <Box>
+                  <Typography align="center" variant="h4" component="div">
+                    Static Art
+                  </Typography>
+                  <img
+                    style={{
+                      height: 300,
+                      width: 300,
+                    }}
+                    src={staticArtImg}
+                  />
+                  <Typography variant="body1" component="div">
+                    Create a static NFT collection. Upload all your images, give
+                    each a name and description. Finally, let us generate your
+                    NFTs via smart contracts and deploy the collection!
+                  </Typography>
+                </Box>
+              </LinkContainer>
+            </Box>
+          </Paper>
+        </Button>
       </Grid>
     </Grid>
   );
