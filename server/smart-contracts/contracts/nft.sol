431bc3035a9fd6eec2eada562a3a6248bc3953e4
R, Alex <alex.richardson19@imperial.ac.uk>
2021-11-08 19:29:10 +0000

    refactor(nft.sol): Make `NFT` extend `Ownable`

--- server/smart-contracts/contracts/nft.sol
+++ server/smart-contracts/contracts/nft.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFT is ERC721Enumerable {
+contract NFT is ERC721Enumerable, Ownable {
   uint256 private _price;
   uint256 private _limit;
   string private _baseURIString;
