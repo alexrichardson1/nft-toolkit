0b1884bf995c259fa845198f62eabf319353ae98
R, Alex <alex.richardson19@imperial.ac.uk>
2021-11-08 19:59:16 +0000

    feat(nft.sol): Override `transferFrom`

--- server/smart-contracts/contracts/nft.sol
+++ server/smart-contracts/contracts/nft.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
+  address public artist;
   uint256 private _price;
   uint256 private _limit;
   string private _baseURIString;
@@ -18,10 +18,12 @@ contract NFT is ERC721Enumerable, Ownable {
   constructor(
     string memory name,
     string memory symbol,
+    address _artist,
     string memory baseURI,
     uint256 limit,
     uint256 price
   ) ERC721(name, symbol) {
+    artist = _artist;
     _limit = limit;
     _price = price;
     _baseURIString = baseURI;
@@ -41,5 +43,22 @@ contract NFT is ERC721Enumerable, Ownable {
       _mint(msg.sender, tokenIdTracker.current());
       tokenIdTracker.increment();
     }
+  }
+
+  /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+  ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+  ///  THEY MAY BE PERMANENTLY LOST
+  /// @param _from The current owner of the NFT
+  /// @param _to The new owner
+  /// @param _tokenId The NFT to transfer
+  function transferFrom(
+    address _from,
+    address _to,
+    uint256 _tokenId
+  ) external payable {
+    require(msg.sender == ownerOf(_tokenId), "You do not own this NFT");
+    require(_from == ownerOf(_tokenId), "You do not own this NFT");
+    // TODO: royalty for artist
+    // TODO: transfer ownership
   }
 }
