dd09e7f2e4b390c45436cfbcde2e93c18d1da77f
Jaimi Patel <jaimip34@gmail.com>
2021-11-17 07:25:32 +0000

    feat(App.tsx): Rename minting page param to `fromAddress`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -41,7 +41,7 @@ const App = (): JSX.Element => {
               </Collapse>
               <Route
                 exact
-                path="/:address([0-9a-zA-Z]{26,})/:collectionName"
+                path="/:fromAddress([0-9a-zA-Z]{26,})/:collectionName"
                 component={MintingPage}
               />
               <Route exact path="/" component={CreateCollectionPage} />
