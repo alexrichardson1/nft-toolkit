965d924d54e891286d51ac1a2c8ec8ac55ef9511
Jaimi Patel <jaimip34@gmail.com>
2021-11-17 07:37:42 +0000

    test(common/): Add false error test for GenericFallback

--- client/src/test/components/common/GenericFallback.test.tsx
+++ client/src/test/components/common/GenericFallback.test.tsx
@@ -5,3 +5,14 @@ test("Generic fallback", () => {
   const tree = mount(<GenericFallback error />);
   expect(tree).toMatchSnapshot();
 });
+
+test("Generic fallback false", () => {
+  const tree = mount(
+    <GenericFallback error={false}>
+      <div>
+        <p>Hello</p>
+      </div>
+    </GenericFallback>
+  );
+  expect(tree).toMatchSnapshot();
+});
