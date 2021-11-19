8d62eb82030f79e53453283ff2d9356d541d89e9
Jaimi Patel <jaimip34@gmail.com>
2021-11-19 06:19:26 +0000

    test(coinGecko.tes.ts): Update tests to getDollarValue

--- client/src/test/utils/coinGecko.test.ts
+++ client/src/test/utils/coinGecko.test.ts
@@ -1,17 +1,17 @@
 import axios from "axios";
-import { getUSDValue } from "utils/coinGecko";
+import { getDollarValue } from "utils/coinGecko";
 jest.mock("axios");
 
 describe("getUSDValue", () => {
   test("Fails to get usd value of native token from invalid chainId", () => {
     axios.get.mockResolvedValue({ data: { ethereum: { usd: 0.0 } } });
     const INVALID_CHAIN = -1;
-    expect(getUSDValue(INVALID_CHAIN)).rejects.toThrow();
+    expect(getDollarValue("100", INVALID_CHAIN)).rejects.toThrow();
   });
 
   test("Gets usd value of native token from chainId", () => {
     axios.get.mockResolvedValue({ data: { ethereum: { usd: 0.0 } } });
     const RINKEBY_ETH = 4;
-    expect(getUSDValue(RINKEBY_ETH)).resolves.toEqual(0.0);
+    expect(getDollarValue("100", RINKEBY_ETH)).resolves.toEqual("($0.00)");
   });
 });
