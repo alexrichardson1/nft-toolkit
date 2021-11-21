30e343360f74fa26a9c516560011f497fc36a6ee
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-11-21 15:14:37 +0000

    feat(global.d.ts): Add reducer actions and payload for layers

--- client/src/lib/global.d.ts
+++ client/src/lib/global.d.ts
@@ -8,33 +8,65 @@ type AnchorT = null | HTMLElement;
 
 type InputEventT = React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>;
 
-type ImageT = { image: File; url: string; name: string; id: string };
+interface ImageI {
+  image: File;
+  url: string;
+  name: string;
+  description?: string;
+}
+
+type ImageT = { [imageId: string]: ImageI };
+
+type LayerImageT = { [imageId: string]: ImageI & { rarity?: number } };
+interface LayerI {
+  name: string;
+  description?: string;
+  images: LayerImageT;
+  numberOfImages: number;
+  precedence: number;
+}
+
+type LayerT = {
+  [layerId: string]: LayerI;
+};
+
 interface FormStateI {
   collectionName: string;
   description: string;
   symbol: string;
-  images: ImageT[];
   mintingPrice: number;
+  static: { numberOfImages: number; images: ImageT };
+  generative: { numberOfLayers: number; layers: LayerT };
 }
+
 type FormActionT =
   | "CHANGE_NAME"
   | "CHANGE_PRICE"
-  | "CHANGE_IMAGES"
   | "CHANGE_SYMBOL"
   | "CHANGE_DESCRIPTION"
+  | "RESET_STATE"
+  | "CHANGE_IMAGES"
   | "CHANGE_IMAGE_NAME"
   | "DELETE_IMAGE"
-  | "RESET_STATE";
+  | "CHANGE_IMAGES_GEN"
+  | "CHANGE_IMAGE_NAME_GEN"
+  | "DELETE_IMAGE_GEN"
+  | "ADD_LAYER"
+  | "CHANGE_PRECEDENCE";
+
 interface FormActionPayloadI {
   newName?: string;
   description?: string;
-  images?: File[];
-  newImgObj?: { newImageName: string; imageId: string };
   price?: string;
-  deleteId?: string;
   symbol?: string;
-  initialState?: FormStateI;
+  newLayer?: { name: string };
+  newImagesGen?: { images: File[]; layerId: string };
+  newImagesStatic?: File[];
+  modifyImgObj?: { newImageName: string; imageId: string };
+  deleteId?: string;
+  precedence?: { oldIndex: number; newIndex: number };
 }
+
 interface FormActionI {
   type: FormActionT;
   payload: FormActionPayloadI;
