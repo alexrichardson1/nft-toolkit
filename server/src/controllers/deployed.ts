fccbf8df2a0244a49768a85b1ce96a203228064e
Jaimi Patel <jaimip34@gmail.com>
2021-11-23 12:35:27 +0000

    feat(controllers/deployed.ts): Update addDeployedAddress for new schema

--- server/src/controllers/deployed.ts
+++ server/src/controllers/deployed.ts
@@ -1,24 +1,29 @@
-import { User } from "@models/user";
+import { Collection } from "@models/collection";
+import { User, UserCollection } from "@models/user";
 import { RequestHandler } from "express";
 
 export const addDeployedAddress: RequestHandler = async (req, res, next) => {
-  const { deployedAddress, fromAddress, collectionName } = req.params;
-  if (!fromAddress || !collectionName || !deployedAddress) {
+  const { creator, chainId, address } = req.params;
+  if (!creator || !chainId || !address) {
     return next(new Error("Invalid params"));
   }
-  const user = await User.findOne({
-    fromAddress: fromAddress,
-  }).exec();
-  if (!user) {
-    return next(new Error("User not found"));
+
+  try {
+    await Collection.findOneAndUpdate(
+      { creator },
+      { address },
+      { sort: { _id: -1 } }
+    ).exec();
+    const userCollection = new UserCollection({
+      address,
+      chainId: parseInt(chainId),
+    });
+    await User.findByIdAndUpdate(creator, {
+      $push: { collections: userCollection },
+    }).exec();
+  } catch (error) {
+    return next(error);
   }
-  const collection = user.collections.find(
-    (col) => col.name === collectionName
-  );
-  if (!collection) {
-    return next(new Error("Collection name not found"));
-  }
-  collection.address = deployedAddress;
-  await user.save();
+
   return res.json({ success: true });
 };
