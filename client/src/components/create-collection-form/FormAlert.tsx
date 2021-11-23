4285502c43b9ee5980fdfdaafb7a726249fb371c
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-11-23 02:04:51 +0000

    refactor(components/): Extracted inner components to outer scope

---
+++ client/src/components/create-collection-form/FormAlert.tsx
@@ -0,0 +1,38 @@
+import CloseIcon from "@mui/icons-material/Close";
+import { Alert, AlertColor, Collapse, IconButton } from "@mui/material";
+import { DEFAULT_ALERT_ELEVATION } from "utils/constants";
+
+const alertContainerStyle = { flexGrow: 1 };
+
+interface PropsT {
+  alertMessage: string;
+  alertSeverity: AlertColor;
+  closeAlert: () => void;
+}
+
+const FormAlert = ({
+  alertMessage,
+  alertSeverity,
+  closeAlert,
+}: PropsT): JSX.Element => (
+  <Collapse sx={alertContainerStyle} in={alertMessage.length !== 0}>
+    <Alert
+      elevation={DEFAULT_ALERT_ELEVATION}
+      variant="filled"
+      data-testid="form-alert"
+      severity={alertSeverity}
+      action={
+        <IconButton
+          aria-label="close"
+          color="inherit"
+          size="small"
+          onClick={closeAlert}>
+          <CloseIcon fontSize="inherit" />
+        </IconButton>
+      }>
+      {alertMessage}
+    </Alert>
+  </Collapse>
+);
+
+export default FormAlert;
