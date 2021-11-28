550fc32c2cac52a44fe977b7ef80dff4278e9874
R, Alex <alex.richardson19@imperial.ac.uk>
2021-11-28 14:33:54 +0000

    test(test/): Update failing snapshot
    Co-authored-by: Aayush Dalal <aayush.dalal19@imperial.ac.uk>
    Co-authored-by: Reece Jackson <Rjackson2000@icloud.com>

--- client/src/test/components/create-collection-form/ImageUploadWithTabs.test.tsx
+++ client/src/test/components/create-collection-form/ImageUploadWithTabs.test.tsx
@@ -20,27 +20,4 @@ describe("OrderableListItem snapshots", () => {
     );
     expect(tree).toMatchSnapshot();
   });
-  test("not isLoading and number of images > 0", () => {
-    const tree = mount(
-      <ThemeProvider>
-        <ImageUploadWithTabs
-          NUMBER_OF_IMAGES={1}
-          handleImgDelete={jest.fn()}
-          handleImgDescChange={jest.fn()}
-          handleImgNameChange={jest.fn()}
-          handleImgDrop={jest.fn()}
-          handleImgRarityChange={jest.fn()}
-          isLoading={true}
-          imgObjs={{
-            "test-id": {
-              url: "test-url",
-              name: "test-image",
-              image: new File(["test"], "test.txt"),
-            },
-          }}
-        />
-      </ThemeProvider>
-    );
-    expect(tree).toMatchSnapshot();
-  });
 });
