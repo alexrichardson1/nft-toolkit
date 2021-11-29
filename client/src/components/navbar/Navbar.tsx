500b973d2a35d47c13305fb17ec8d46975f5fbea
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-11-29 18:59:05 +0000

    chore(Navbar.tsx): Add `SidebarMenu.tsx` to Navbar
    Co-authored-by: Reece Jackson <Rjackson2000@icloud.com>

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
+import MenuIcon from "@mui/icons-material/Menu";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import { PopoverOrigin } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
 import Fab from "@mui/material/Fab";
 import IconButton from "@mui/material/IconButton";
 import { useTheme } from "@mui/material/styles";
@@ -12,11 +14,13 @@ import Typography from "@mui/material/Typography";
 import NetworkSpeedDial from "components/common/Networks";
 import Wallet from "components/wallet/Wallet";
 import ThemeContext from "context/theme/ThemeContext";
+import * as React from "react";
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { NAVBAR_HEIGHT } from "utils/constants";
 import getComponentByMode from "utils/getComponentByMode";
 import MobileMenu from "../common/MobileMenu";
+import SidebarMenu from "./SidebarMenu";
 
 const MENU_ANCHOR_ORIGIN: PopoverOrigin = {
   vertical: "top",
@@ -24,12 +28,10 @@ const MENU_ANCHOR_ORIGIN: PopoverOrigin = {
 };
 
 const moreIconContainerStyle = { display: { xs: "flex", md: "none" } };
-
 const toolbarStyle = {
   width: 1,
   height: 1,
-  justifyContent: "center",
-  alignItems: "center",
+  gap: 1,
 };
 const navOptionsStyle = {
   display: { xs: "none", md: "flex" },
@@ -49,6 +51,20 @@ const Navbar = (): JSX.Element => {
   const theme = useTheme();
   const { toggleColourMode } = useContext(ThemeContext);
   const [mobileAnchorEl, setMobileAnchorEl] = useState<AnchorT>(null);
+  const [drawerOpen, setDrawerState] = useState(false);
+
+  const handleDrawerToggle = (
+    event: React.KeyboardEvent | React.MouseEvent
+  ) => {
+    if (
+      event.type === "keydown" &&
+      ((event as React.KeyboardEvent).key === "Tab" ||
+        (event as React.KeyboardEvent).key === "Shift")
+    ) {
+      return;
+    }
+    setDrawerState((prev) => !prev);
+  };
 
   const handleMobileMenuClose = () => setMobileAnchorEl(null);
   const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) =>
@@ -89,30 +105,43 @@ const Navbar = (): JSX.Element => {
   );
 
   return (
-    <Box flexGrow={1}>
-      <AppBar sx={appBarStyle} position="sticky">
-        <Toolbar sx={toolbarStyle}>
-          <Typography
-            sx={{ textDecoration: "none" }}
-            component={Link}
-            to="/"
-            color="primary"
-            variant="h3"
-            noWrap>
-            NFToolkit
-          </Typography>
-          <Box flexGrow={1} />
-          {navOptions}
-          {moreOptionsIcon}
-        </Toolbar>
-      </AppBar>
-      <MobileMenu
-        isOpen={IS_MOBILE_MENU_OPEN}
-        anchOrigin={MENU_ANCHOR_ORIGIN}
-        anchorEl={mobileAnchorEl}
-        handleClose={handleMobileMenuClose}
-      />
-    </Box>
+    <>
+      <Drawer anchor="left" open={drawerOpen} onClose={handleDrawerToggle}>
+        <SidebarMenu handleDrawerToggle={handleDrawerToggle} />
+      </Drawer>
+      <Box flexGrow={1}>
+        <AppBar sx={appBarStyle} position="sticky">
+          <Toolbar sx={toolbarStyle}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="drawer-menu"
+              onClick={handleDrawerToggle}>
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              sx={{ textDecoration: "none" }}
+              component={Link}
+              to="/"
+              color="primary"
+              variant="h3"
+              noWrap>
+              NFToolkit
+            </Typography>
+            <Box flexGrow={1} />
+            {navOptions}
+            {moreOptionsIcon}
+          </Toolbar>
+        </AppBar>
+        <MobileMenu
+          isOpen={IS_MOBILE_MENU_OPEN}
+          anchOrigin={MENU_ANCHOR_ORIGIN}
+          anchorEl={mobileAnchorEl}
+          handleClose={handleMobileMenuClose}
+        />
+      </Box>
+    </>
   );
 };
 
