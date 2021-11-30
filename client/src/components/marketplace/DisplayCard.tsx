0af1f118b3e36fbe9983d5f16d68e8f0a6b20ed3
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-11-30 19:12:24 +0000

    feat(DisplayCard.tsx): Add css styling for card

--- client/src/components/marketplace/DisplayCard.tsx
+++ client/src/components/marketplace/DisplayCard.tsx
@@ -1,58 +1,57 @@
+import { Skeleton } from "@mui/material";
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActionArea from "@mui/material/CardActionArea";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import "./displaycard.css";
 
 interface PropsT {
   data: CardInfoI;
+  loading?: boolean;
 }
 
-const dummy: CardInfoI = {
-  name: "APE 0",
-  description: "",
-  image:
-    "https://media4.giphy.com/media/30NdmWTV6ORPuwUNUu/giphy.gif?cid=ecf05e47auja74quh6utc1i80lc2j3zfqbr15vnql5o5tg3r&rid=giphy.gif&ct=g",
-  // eslint-disable-next-line camelcase
-  attributes: { tier: "Legendary" },
-};
-
-const DisplayCard = (_props: PropsT): JSX.Element => {
+const DisplayCard = ({ data, loading }: PropsT): JSX.Element => {
   return (
-    <Card sx={{ maxHeight: 400, maxWidth: 300 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="200"
-          image={dummy.image}
-          alt={dummy.name}
-        />
-        <CardContent>
-          <Box display="flex" justifyContent="space-between">
-            <Typography
-              gutterBottom={dummy.description.length > 0}
-              variant="h5"
-              color="primary">
-              {dummy.name}
-            </Typography>
-            <Typography variant="h5">{dummy.attributes.tier}</Typography>
-          </Box>
-          <Typography
-            sx={{
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              display: "-webkit-box",
-              lineClamp: 2,
-              WebkitLineClamp: "5",
-              WebkitBoxOrient: "vertical",
-            }}
-            variant="body2">
-            {dummy.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Box tabIndex={0} className="marketplace-card">
+      <Box
+        sx={{ bgcolor: "background.paper" }}
+        className="marketplace-card-container">
+        {loading ? (
+          <>
+            <Skeleton
+              height="100%"
+              variant="rectangular"
+              className="card-front"
+            />
+            <Skeleton
+              height="100%"
+              variant="rectangular"
+              className="card-back"
+            />
+          </>
+        ) : (
+          <>
+            <img
+              src={data.image}
+              alt={data.name}
+              className="card-img card-front"
+            />
+            <Box className="card-back">
+              <Box className="card-title-container">
+                <Typography noWrap variant="h5" color="primary">
+                  {data.name}
+                </Typography>
+                <Typography noWrap variant="h6">
+                  {/* eslint-disable-next-line dot-notation */}
+                  {data.attributes["tier"]}
+                </Typography>
+              </Box>
+              <Typography className="card-description" variant="body2">
+                {data.description}
+              </Typography>
+            </Box>
+          </>
+        )}
+      </Box>
+    </Box>
   );
 };
 
