beafd3f778c720fd47a9f4ce905e2c40baec27cd
Jaimi Patel <jaimip34@gmail.com>
2021-12-03 03:22:53 +0000

    feat(routes/metadata.ts): Add route for collection metadata

--- server/src/routes/metadata.ts
+++ server/src/routes/metadata.ts
@@ -1,13 +1,21 @@
 import { errorHandler } from "@controllers/common";
-import { getTokenMetadata } from "@controllers/metadata";
-import { metadataValidator } from "@validators/metadata";
+import { getCollectionMetadata, getTokenMetadata } from "@controllers/metadata";
+import { invalidTokenId, metadataValidator } from "@validators/metadata";
 import { Router as router } from "express";
 
 const metadataRoutes = router();
 
 metadataRoutes.get(
+  "/:chainId/:address/contract/data",
+  metadataValidator,
+  errorHandler,
+  getCollectionMetadata
+);
+
+metadataRoutes.get(
   "/:chainId/:address/:tokenId",
   metadataValidator,
+  invalidTokenId,
   errorHandler,
   getTokenMetadata
 );
