6664aba25ff2ea157fb4552575adcf52bd4a4678
Jaimi Patel <jaimip34@gmail.com>
2021-12-03 03:42:50 +0000

    feat(PurchasePage.tsx): Connect to contract

--- client/src/components/marketplace/PurchasePage.tsx
+++ client/src/components/marketplace/PurchasePage.tsx
@@ -1,19 +1,25 @@
+import { Collapse, Grid } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { SxProps } from "@mui/system/styleFunctionSx";
+import { ProgressActions } from "actions/progressActions";
+import axios from "axios";
+import useAppDispatch from "hooks/useAppDispatch";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { NFT__factory as NftFactory } from "typechain";
+import { getRPCProvider } from "utils/mintingPageUtils";
 import DisplayCard from "./DisplayCard";
 
-const dummyData: CollectionI = {
+const dummyData: TokenI = {
   name: "APE 1",
   description:
     "This is a collection where you can do something and get something done but also a collection where you can not do something and get nothing done because if you think about it carefully, you can basically get everything but nothing done at the same time which is a bit annoying.",
-  image: "https://pbs.twimg.com/media/FFaFHBNXMAAR41l.jpg",
+  image: "",
   attributes: { tier: "Legendary" },
   id: 0,
   price: "69",
@@ -29,91 +35,114 @@ const paperStyle: SxProps = {
 };
 
 const PurchasePage = (): JSX.Element => {
-  const { paramChainId } = useParams<ParamsI>();
+  const { paramChainId, tokenId, address } = useParams<
+    ParamsI & { tokenId: string }
+  >();
+  const dispatch = useAppDispatch();
+  const [token, setToken] = useState<TokenI>(dummyData);
+  const [symbol, setSymbol] = useState("");
+
+  useEffect(() => {
+    async function getTokenData() {
+      dispatch({ type: ProgressActions.START_PROGRESS, payload: {} });
+      const nftContract = NftFactory.connect(
+        address,
+        getRPCProvider(parseInt(paramChainId))
+      );
+      dispatch({
+        type: ProgressActions.ADVANCE_PROGRESS_BY,
+        payload: { advanceProgressBy: 50 },
+      });
+
+      const tokenURI = await nftContract.tokenURI(tokenId);
+      const res = await axios.get(tokenURI);
+      res.data.id = parseInt(tokenId);
+      const attributeMap: AttributeI = {};
+      res.data.attributes.forEach((attr: ContractAttributeI) => {
+        attributeMap[attr.trait_type] = attr.value;
+      });
+      res.data.attributes = attributeMap;
+      const token = res.data;
+      setToken(token);
+
+      const symbol = await nftContract.symbol();
+      setSymbol(symbol);
+
+      dispatch({ type: ProgressActions.FINISH_PROGRESS, payload: {} });
+      dispatch({ type: ProgressActions.STOP_PROGRESS, payload: {} });
+    }
+    getTokenData();
+  }, []);
 
   return (
-    <Box alignItems="center" flexGrow={1} display="flex" gap={10}>
-      <Box gap={2} display="flex" flexWrap="wrap" justifyContent="center">
-        <DisplayCard
-          chainId={Number(paramChainId)}
-          to="/purchase"
-          key={dummyData.id}
-          data={dummyData}
-        />
-      </Box>
-      <Box sx={paperStyle}>
-        <Box display="flex" flexDirection="column" alignItems="center">
-          <ListItem>
+    <Collapse sx={{ width: 1 }} in={token !== dummyData}>
+      <Box alignItems="center" flexGrow={1} display="flex" gap={10}>
+        <Box gap={2} display="flex" flexWrap="wrap" justifyContent="center">
+          <DisplayCard
+            chainId={Number(paramChainId)}
+            to={`/${paramChainId}/${address}/${tokenId}`}
+            key={token.id}
+            data={token}
+          />
+        </Box>
+        <Box sx={paperStyle}>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <ListItem>
+              <Box>
+                <Typography
+                  textAlign="left"
+                  variant="h3"
+                  color="secondary"
+                  fontWeight="medium">
+                  {token.name}
+                </Typography>
+                <Typography
+                  textAlign="left"
+                  color="primary"
+                  fontStyle="oblique">
+                  {symbol}
+                </Typography>
+              </Box>
+            </ListItem>
+            <Divider variant="middle" flexItem={true} />
+            <Divider variant="middle" />
+            <ListItem>
+              <Grid container>
+                {Object.entries(token.attributes).map((attr, index) => {
+                  const [key, value] = attr;
+                  return (
+                    <Grid item xs={12} sm={6} md={4} key={index}>
+                      <ListItem key={index}>
+                        <Button
+                          fullWidth
+                          variant="outlined"
+                          color="info"
+                          border-radius="5px">
+                          <ListItemText primary={key} secondary={value} />
+                        </Button>
+                      </ListItem>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </ListItem>
+            <Divider variant="middle" />
+            <ListItem>
+              <Box>
+                <Typography textAlign="left" color="secondary">
+                  Floor Price: xx ETH
+                </Typography>
+              </Box>
+            </ListItem>
             <Box>
-              <Typography
-                textAlign="left"
-                variant="h3"
-                color="secondary"
-                fontWeight="medium">
-                NFT name...
-              </Typography>
-              <Typography textAlign="left" color="primary" fontStyle="oblique">
-                Creator/s...
-              </Typography>
+              <Button variant="contained" size="large" border-radius="10px">
+                Purchase
+              </Button>
             </Box>
-          </ListItem>
-          <Divider variant="middle" flexItem={true} />
-          <ListItem>
-            <Button
-              variant="contained"
-              size="medium"
-              border-radius="10px"
-              disabled>
-              Rarity...
-            </Button>
-          </ListItem>
-          <Divider variant="middle" />
-          <ListItem>
-            <Box>
-              <Stack direction="row" spacing={3}>
-                <Button variant="outlined" color="info" border-radius="5px">
-                  <ListItem>
-                    <ListItemText
-                      primary="Attribute 1"
-                      secondary="Attr name 1"
-                    />
-                  </ListItem>
-                </Button>
-                <Button variant="outlined" color="info" border-radius="5px">
-                  <ListItem>
-                    <ListItemText
-                      primary="Attribute 2"
-                      secondary="Attr name 2"
-                    />
-                  </ListItem>
-                </Button>
-                <Button variant="outlined" color="info" border-radius="5px">
-                  <ListItem>
-                    <ListItemText
-                      primary="Attribute 3"
-                      secondary="Attr name 3"
-                    />
-                  </ListItem>
-                </Button>
-              </Stack>
-            </Box>
-          </ListItem>
-          <Divider variant="middle" />
-          <ListItem>
-            <Box>
-              <Typography textAlign="left" color="secondary">
-                Floor Price: xx ETH
-              </Typography>
-            </Box>
-          </ListItem>
-          <Box>
-            <Button variant="contained" size="large" border-radius="10px">
-              Purchase
-            </Button>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </Collapse>
   );
 };
 
