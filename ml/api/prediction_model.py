db28d2b839770ccc29a59d27f3014609c6ca5e65
Devam Savjani <devamsavjani@rocketmail.com>
2021-12-30 12:01:58 +0000

    feat(prediction_model.py): Use reddit and twitter to cluster collections

--- ml/api/prediction_model.py
+++ ml/api/prediction_model.py
@@ -2,59 +2,86 @@
 Model for clustering collections
 """
 
+import math
+import pickle
 import distance
 from sklearn.cluster import AffinityPropagation
 import numpy as np
 
 
-class Prediction_Model:  # pylint: disable=invalid-name
+class PredictionModel:
     """
     Model for clustering collections
     """
 
-    def __init__(self, collection_names):
-        self.collection_names = np.array(collection_names)
+    def __init__(self, collections):
+        self.collections = np.array(collections)
         self.lev_similarity = []
         self.model = None
+
+    def set_collections(self, collections):
+        """
+        Set the collections numpy array
+        """
+        self.collections = collections
 
     def train(self, damping=0.6, max_iter=400):
         """
         Train model for clustering collections
         """
-        print("Computing Levenshtien Matrix")
-        self.lev_similarity = -1 * np.array([[distance.levenshtein(c1, c2)
-                                              for c1 in self.collection_names]
-                                             for c2 in self.collection_names])
+        print("Computing Similarity Matrix")
+        self.lev_similarity = -1 * np.array([[
+            round(0.3 * distance.levenshtein(c1["name"], c2["name"])) +
+            math.log10(abs(c1["reddit_members"] - c2["reddit_members"]) + 1) +
+            math.log10(abs(c1["twitter_followers"] -
+                       c2["twitter_followers"]) + 1)
+            for c1 in self.collections] for c2 in self.collections])
         print("Starting Model")
         self.model = AffinityPropagation(
-            affinity="precomputed", damping=damping, max_iter=max_iter, verbose=True)
+            affinity="precomputed",
+            damping=damping,
+            max_iter=max_iter,
+            verbose=True)
         self.model.fit(self.lev_similarity)
 
-    def predict(self, word):
+    def predict(self, word, reddit_members=0, twitter_followers=0):
         """
         Predict using Model
         """
         lev_similarity = np.array([
-            distance.levenshtein(
-                self.collection_names[self.model.cluster_centers_indices_[cluster_id]], word)
-            for cluster_id in np.unique(self.model.labels_)])
+            round(
+                0.3 * distance.levenshtein(
+                    self.collections[self.model.cluster_centers_indices_[cluster_id]]["name"], word)
+            ) +
+            math.log10(
+                abs(self.collections[self.model.cluster_centers_indices_[
+                    cluster_id]]["reddit_members"] - reddit_members)
+            ) +
+            math.log10(
+                abs(self.collections[self.model.cluster_centers_indices_[
+                    cluster_id]]["twitter_followers"] - twitter_followers)
+            ) for cluster_id in np.unique(self.model.labels_)])
         (index, _) = min(enumerate(lev_similarity), key=lambda x: x[1])
-        res = np.unique(self.collection_names[np.nonzero(
-            self.model.labels_ == np.unique(self.model.labels_)[index])])
-        result = []
-        for i, _ in enumerate(res):
-            result.append((res[i], distance.levenshtein(res[i], word)))
-        result.sort(key=lambda x: x[1])
-        return result[:6]
+        return np.unique([col["name"] for col in self.collections[np.nonzero(
+            self.model.labels_ == np.unique(self.model.labels_)[index])]])[:6]
+
+    def save_model(self):
+        """
+        Save the model
+        """
+        with open('collection_model', 'wb') as file:
+            pickle.dump(self, file)
 
     def __str__(self):
         res = ""
         for i, cluster_id in enumerate(np.unique(self.model.labels_)):
-            cluster_head = self.collection_names[self.model.cluster_centers_indices_[
+            cluster_head = self.collections[self.model.cluster_centers_indices_[
                 cluster_id]]
-            cluster = np.unique(
-                self.collection_names[np.nonzero(self.model.labels_ == cluster_id)])
+            dict_cluster = np.array(
+                self.collections[np.nonzero(self.model.labels_ == cluster_id)])
+            cluster = np.unique([col["name"] for col in dict_cluster])
             cluster_str = ", ".join(cluster)
-            res += str(f"cluster {i} - *{cluster_head}:* {cluster_str}")
+            res += str("cluster %i - *%s:* %s" %
+                       (i, cluster_head, cluster_str))
             res += "\n"
         return res
