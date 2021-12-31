f405414273e1e0be85924669a1eb9d6a62fc3fae
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-12-31 15:02:21 +0000

    feat(src/): Add route for MyCollectionsPage

--- client/src/pages/MyCollectionsPage.tsx
+++ client/src/pages/MyCollectionsPage.tsx
@@ -1,5 +1,18 @@
+import Stack from "@mui/material/Stack";
+import PageHeader from "components/common/PageHeader";
+import { useParams } from "react-router-dom";
+
+interface ParamsI {
+  address: string;
+}
+
 const MyCollectionsPage = (): JSX.Element => {
-  return <div></div>;
+  const { address } = useParams<ParamsI>();
+  return (
+    <Stack>
+      <PageHeader text={`${address}'s Collection`} />
+    </Stack>
+  );
 };
 
 export default MyCollectionsPage;
