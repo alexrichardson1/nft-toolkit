cc1a5823e035412f448d453be71c1edf4f447cc6
Jaimi Patel <jaimip34@gmail.com>
2021-12-31 16:28:27 +0000

    feat(ImageUpload.tsx): Set max image size to 50mb

--- client/src/components/create-collection-form/custom-image-upload/ImageUpload.tsx
+++ client/src/components/create-collection-form/custom-image-upload/ImageUpload.tsx
@@ -76,7 +76,7 @@ const ImageUpload = ({
       <input
         ref={ref}
         accept="image/*"
-        max-size="100mb"
+        max-size="50mb"
         onChange={(e) => handleImageDrop(e, e.target.files)}
         id={UPLOAD_INPUT_ID}
         className="img-upload-input"
