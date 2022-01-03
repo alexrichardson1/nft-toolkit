2a97c8be001ec505fd28c75ea0d934ba4f625d8b
Jaimi Patel <jaimip34@gmail.com>
2022-01-03 02:48:43 +0000

    feat(constants.ts): Remove solana & cardano

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,10 +1,8 @@
 import { S3 } from "aws-sdk";
 import AvalancheLogo from "images/avalanche-logo.svg";
 import BinanceLogo from "images/binance-logo.svg";
-import CardanoLogo from "images/cardano-logo.svg";
 import EthereumLogo from "images/ethereum-logo.svg";
 import PolygonLogo from "images/polygon-logo.svg";
-import SolanaLogo from "images/solana-logo.svg";
 
 export const DEFAULT_PADDING = 3;
 export const DEFAULT_GAP = "10px";
@@ -16,9 +14,7 @@ export const NAVBAR_HEIGHT = 90;
 
 export const NETWORKS: NetworkT[] = [
   { name: "Ethereum", icon: EthereumLogo, chainId: 4 },
-  { name: "Cardano", icon: CardanoLogo },
   { name: "Avalanche", icon: AvalancheLogo, chainId: 43114 },
-  { name: "Solana", icon: SolanaLogo },
   { name: "Polygon", icon: PolygonLogo, chainId: 137 },
   { name: "BSC", icon: BinanceLogo, chainId: 56 },
 ];
