e9013e2fa9552c3a0fbce45b3daa961c5514c40f
Devam Savjani <devamsavjani@rocketmail.com>
2022-01-03 23:24:29 +0000

    feat(prediction_model_abstract): Add abstract class for model

--- ml/api/models/prediction_model_nearest_neighbors.py
+++ ml/api/models/prediction_model_nearest_neighbors.py
@@ -2,51 +2,20 @@
 Model for clustering collections
 """
 
-import math
 from select_model import get_distance
 import distance
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+import prediction_model_abstract
 
 
-class PredictionModelNearestNeighbors:
+class PredictionModelNearestNeighbors(prediction_model_abstract.PredictionModel):
     """
     Model for clustering collections
     """
 
-    def __init__(self, collections):
-        self.data = pd.DataFrame(
-            {'name': pd.Series(dtype='str'),
-             'reddit_score': pd.Series(dtype='float'),
-             'twitter_score': pd.Series(dtype='float'),
-             'avg_sale_price': pd.Series(dtype='float'),
-             'volume': pd.Series(dtype='float'),
-             'preview_img': pd.Series(dtype='str')})
-
-        for collection in collections:
-            if collection['avg_sale_price'] != 0:
-                self.data = self.data.append(
-                    {'name': collection['name'],
-                     'reddit_score': collection['reddit_score'],
-                     'twitter_score': collection['twitter_score'],
-                     'avg_sale_price': collection['avg_sale_price'],
-                     'volume': collection['volume'],
-                     'preview_img': collection['preview_img']}, ignore_index=True)
-
-        self.x_training, testing = train_test_split(self.data.drop(
-            columns=['name', 'avg_sale_price', 'volume', 'preview_img']), test_size=0.4)
-        self.x_validation, self.x_testing = train_test_split(
-            testing, test_size=0.5)
-        self.scaler = StandardScaler()
-
-    def preprocess_data(self):
-        """
-        Preprocess data passed in
-        """
-        x_scaled = self.scaler.fit_transform(self.x_training)
-        self.x_training = pd.DataFrame(x_scaled)
+    def train(self):
+        pass
 
     def predict(self, word, reddit_score=0, twitter_score=0):
         """
@@ -68,33 +37,10 @@ class PredictionModelNearestNeighbors:
 
         return self.data.iloc[dataset.sort_values('distance').index[:16]].to_dict(orient='records')
 
-    def get_rmse(self):
-        """
-        Gets mean squared error of model
-        """
-        mse = 0
-        total_validated = 0
-        for i in self.x_validation.index:
-            row = self.data.iloc[i]
-            name = row['name']
-            price = row['avg_sale_price']
-            print("Getting accuracy of " + name)
-            predictions = self.predict(
-                name, row['reddit_score'], row['twitter_score'])
-
-            pred_avg_price = sum([col['avg_sale_price']
-                                  for col in predictions]) / len(predictions)
-            print("Got prediction avg price of " + str(pred_avg_price))
-            print("Actual price of " + str(price))
-            total_validated += 1
-            mse += math.pow(pred_avg_price - price, 2)
-            print("-----------------")
-        return math.sqrt(mse / total_validated)
-
-
+# import sys
 # sys.path.insert(1, "api/")
-# from routes import get_collection
 
+# from routes import get_collection
 
 # db_collection = get_collection()
 # collections = db_collection.find({})
