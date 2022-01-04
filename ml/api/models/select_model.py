5da8f968c541983bc1dd3ae857baebffa9bd71dd
Devam Savjani <devamsavjani@rocketmail.com>
2022-01-04 01:40:51 +0000

    feat(select_model): Add script to get best model

--- ml/api/models/select_model.py
+++ ml/api/models/select_model.py
@@ -2,11 +2,39 @@
 Script to find best model
 """
 
-import math
+import sys
+from prediction_model_affinity_prop_name_score import PredictionModelAffinityPropagationNamingScore
+from prediction_model_affinity_prop_name import PredictionModelAffinityPropagationNaming
+from prediction_model_kmeans import PredictionModelKMeans
+from prediction_model_nearest_neighbors import PredictionModelNearestNeighbors
 
+sys.path.insert(1, "api/")
 
-def get_distance(df1, df2):
-    """
-    Calculate Euclideun distance ^2 between to dataframes
-    """
-    return math.pow(df1[0][0] - df2[0], 2) + math.pow(df1[0][1] - df2[1], 2)
+from routes import get_collection  # noqa # pylint:disable=import-error, wrong-import-position
+
+db_collection = get_collection()
+collections = db_collection.find({})
+
+model1 = PredictionModelAffinityPropagationNamingScore(list(collections), 0.4)
+model2 = PredictionModelAffinityPropagationNaming(list(collections))
+model3 = PredictionModelKMeans(list(collections))
+model4 = PredictionModelNearestNeighbors(list(collections))
+
+model1.preprocess_data()
+model1.train()
+
+model2.preprocess_data()
+model2.train()
+
+model3.preprocess_data()
+model3.train()
+
+model2.save_model()
+
+model4.preprocess_data()
+model4.train()
+
+print(f"Model1: RMSE is {model1.get_rmse()}")
+print(f"Model2: RMSE is {model2.get_rmse()}")
+print(f"Model3: RMSE is {model3.get_rmse()}")
+print(f"Model4: RMSE is {model4.get_rmse()}")
