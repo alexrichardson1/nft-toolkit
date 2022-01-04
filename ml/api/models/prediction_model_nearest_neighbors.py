3d1516b483fc4e358fc19564861dae0292ddee38
Devam Savjani <devamsavjani@rocketmail.com>
2022-01-04 20:33:24 +0000

    feat(prediction_model_nearest_neighbors): Add hyperparams

--- ml/api/models/prediction_model_nearest_neighbors.py
+++ ml/api/models/prediction_model_nearest_neighbors.py
@@ -13,6 +13,11 @@ class PredictionModelNearestNeighbors(prediction_model_abstract.PredictionModel)
     Model for clustering collections
     """
 
+    def __init__(self, collections, levenshtein_scale=0.5, score_scaler=0.5):
+        super().__init__(collections)
+        self.levenshtein_scale = levenshtein_scale
+        self.score_scaler = score_scaler
+
     def train(self):
         pass
 
@@ -29,10 +34,11 @@ class PredictionModelNearestNeighbors(prediction_model_abstract.PredictionModel)
 
         for i in self.x_training.index:
             data_row = self.data.iloc[i]
-            distance_to_point = distance.levenshtein(word, data_row['name']) + \
-                np.sqrt(prediction_model_abstract.get_distance(
+            distance_to_point = self.levenshtein_scale * \
+                distance.levenshtein(word, data_row['name']) + \
+                self.score_scaler * np.sqrt(prediction_model_abstract.get_distance(
                     scaled_row, self.x_training.iloc[i]))
             dataset = dataset.append(
                 {'distance': distance_to_point, 'index': i}, ignore_index=True)
 
-        return self.data.iloc[dataset.sort_values('distance').index[:16]].to_dict(orient='records')
+        return self.data.iloc[dataset.sort_values('distance').index[:6]].to_dict(orient='records')
