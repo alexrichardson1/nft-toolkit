07d5320533231fb0835c7848e759919322042f67
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2022-01-05 22:04:52 +0000

    chore(TierSelectionStep.tsx): Add styling and icons

--- client/src/components/create-collection-form/form-steps/TierSelectionStep.tsx
+++ client/src/components/create-collection-form/form-steps/TierSelectionStep.tsx
@@ -1,5 +1,7 @@
 import { DragEndEvent } from "@dnd-kit/core";
-import { Collapse } from "@mui/material";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { Collapse, Stack, Typography } from "@mui/material";
 import OrderableList from "components/common/OrderableList";
 import OrderableListInput from "components/common/OrderableListInput";
 import OrderableListItem from "components/common/OrderableListItem";
@@ -59,7 +61,15 @@ const TierSelectionStep = ({
         <RarityProgressBar totalRarity={state.generative.totalTierRarity} />
       </Collapse>
       <Collapse in={state.generative.numberOfTiers > 1}>
-        <p>Rarest Tier</p>
+        <Stack
+          justifyContent="center"
+          spacing={1}
+          direction="row"
+          alignItems="center">
+          <ArrowDownwardIcon />
+          <Typography variant="h5">Rarest Tier</Typography>
+          <ArrowDownwardIcon />
+        </Stack>
       </Collapse>
       <OrderableList
         handleItemReorder={handleTierReorder}
@@ -80,9 +90,16 @@ const TierSelectionStep = ({
         ))}
       </OrderableList>
       <Collapse in={state.generative.numberOfTiers > 1}>
-        <p>Most Common Tier</p>
+        <Stack
+          justifyContent="center"
+          spacing={1}
+          direction="row"
+          alignItems="center">
+          <ArrowUpwardIcon />
+          <Typography variant="h5">Most Common Tier</Typography>
+          <ArrowUpwardIcon />
+        </Stack>
       </Collapse>
-
       <OrderableListInput
         onKeyPress={handleInputKeyPress}
         text={text}
