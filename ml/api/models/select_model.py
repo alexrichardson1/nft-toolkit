611ae658348188149895bbed7943f63974e330a6
Devam Savjani <devamsavjani@rocketmail.com>
2022-01-06 03:20:32 +0000

    fix(select_model): Fix nn search

--- ml/api/models/select_model.py
+++ ml/api/models/select_model.py
@@ -40,13 +40,13 @@ def get_rmse_for_affinity_name_score():
         model3.train()
         print(
             f"AFFINITY WITH SCORE: lev_scale={lev_scale}," +
-            " scaler_func={math.log} ----- RMSE:{model1.get_rmse()}")
+            f" scaler_func={math.log} ----- RMSE:{model1.get_rmse()}")
         print(
             f"AFFINITY WITH SCORE: lev_scale={lev_scale}," +
-            " scaler_func={np.sqrt} ----- RMSE:{model2.get_rmse()}")
+            f" scaler_func={np.sqrt} ----- RMSE:{model2.get_rmse()}")
         print(
             f"AFFINITY WITH SCORE: lev_scale={lev_scale}," +
-            " scaler_func={(lambda n: n)} ----- RMSE:{model3.get_rmse()}")
+            f" scaler_func={(lambda n: n)} ----- RMSE:{model3.get_rmse()}")
 
 
 def get_rmse_for_affinity_name():
@@ -74,15 +74,15 @@ def get_rmse_for_nn():
     """
     Thread body to get all RMSEs using PredictionModelNearestNeighbors
     """
-    for lev_scale in np.arange(0, 0.2, 5):
-        for score_scale in np.arange(0, 0.2, 5):
+    for lev_scale in np.arange(0.5, 2.5, 0.5):
+        for score_scale in np.arange(0.5, 3, 0.5):
             model = PredictionModelNearestNeighbors(
                 collections, lev_scale, score_scale)
             model.preprocess_data()
             model.train()
             print(
                 f"NEAREST NEIGHBORS: lev_scale={lev_scale}" +
-                " score_scale={score_scale} ----- RMSE:{model.get_rmse()}")
+                f" score_scale={score_scale} ----- RMSE:{model.get_rmse()}")
 
 
 def get_rmse_for_birch():
