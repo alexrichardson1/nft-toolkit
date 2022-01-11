7f715eafc8fd850cb28ddbfb07f5f2efda1385e8
Devam Savjani <devamsavjani@rocketmail.com>
2022-01-11 01:33:08 +0000

    test(test_hype_meter): Mock getenv

--- ml/test/test_hype_meter.py
+++ ml/test/test_hype_meter.py
@@ -14,6 +14,7 @@ mockHypeMeter3 = HypeMeter("cyberPunks", None, "twitter")
 mockHypeMeter4 = HypeMeter("cyberPunks", "reddit", "twitter")
 
 
+@patch("os.getenv", return_value="foo")
 class TestHypeMeter(unittest.TestCase):
     """
     Test Class for Hype Meter
@@ -28,13 +29,14 @@ class TestHypeMeter(unittest.TestCase):
         self.mock_hype_meter3 = HypeMeter("cyberPunks", None, "twitter")
         self.mock_hype_meter4 = HypeMeter("cyberPunks", "reddit", "twitter")
 
-    def test_get_num_of_twitter_followers_user1(self):
+    def test_get_num_of_twitter_followers_user1(self, mock_get_env):
         """
         Returns 0 if no Twitter handle
         """
         assert self.mock_hype_meter1.get_num_of_twitter_followers() == 0
+        assert not mock_get_env.called
 
-    def test_get_num_of_twitter_followers_user3(self):
+    def test_get_num_of_twitter_followers_user3(self, mock_get_env):
         """
         Successfully returns correct number from API
         """
@@ -42,16 +44,20 @@ class TestHypeMeter(unittest.TestCase):
             mocked_request.return_value.text = '{"followers_count":10}'
             mocked_request.return_value.status_code = 200
             assert self.mock_hype_meter3.get_num_of_twitter_followers() == 10
+            assert mock_get_env.called
 
-    def test_get_num_of_twitter_followers_user4_failed_request(self):
+    # pylint:disable=unused-argument
+    def test_get_num_of_twitter_followers_user4_failed_request(self, mock_get_env):
         """
         Returns 0 if API call fails
         """
         with patch('hype_meter.requests.request') as mocked_request:
             mocked_request.return_value.status_code = 404
             assert self.mock_hype_meter4.get_num_of_twitter_followers() == 0
+            assert mock_get_env.called
 
-    def test_get_num_of_twitter_followers_user4_followers_count_not_int_response(self):
+    # pylint:disable=unused-argument
+    def test_get_num_of_twitter_followers_user4_followers_count_not_in_response(self, mock_get_env):
         """
         Returns 0 if API response does  not contain followers_count
         """
@@ -59,6 +65,7 @@ class TestHypeMeter(unittest.TestCase):
             mocked_request.return_value.text = '{"foo":10}'
             mocked_request.return_value.status_code = 200
             assert self.mock_hype_meter4.get_num_of_twitter_followers() == 0
+            assert mock_get_env.called
 
 
 if __name__ == '__main__':
